aaa706526f80c869f683c57313d9f01656b8dd69
iadgovuser11 <iadgovuser11@users.noreply.github.com>
2017-02-13 06:56:22 -0500

    Switched playbook libraries
    Added Cytoscape and removed Treant for playbook editor

--- server/static/pages/playbook/js/main.js
+++ server/static/pages/playbook/js/main.js
@@ -2,170 +2,69 @@ $("#executeWorkflowButton").on("click", function(e){
     notifyMe();
 })
 
-
-// fix arrow end issues:
-// https://github.com/DmitryBaranovskiy/raphael/issues/471
-
-var chart_config = {
-	chart: {
-		container: "#OrganiseChart-big-commpany",
-		levelSeparation: 45,
-
-		rootOrientation: "WEST",
-
-		nodeAlign: "BOTTOM",
-
-		connectors: {
-			type: "step",
-			style: {
-				"stroke-width": 2
-			}
-		},
-		node: {
-			HTMLclass: "big-commpany"
-		}
-	},
-
-	nodeStructure: {
-		text: { name: "CEO" },
-		connectors: {
-			style: {
-				'stroke': '#bbb',
-				'arrow-end': 'oval-wide-long'
-			}
-		},
-		children: [
-			{
-				text: { name: "Account" },
-				stackChildren: true,
-				connectors: {
-					style: {
-						'stroke': '#8080FF',
-						'arrow-end': 'block-wide-long'
-					}
-				},
-				children: [
-					{
-						text: {name: "Receptionist"},
-						HTMLclass: "reception"
-					},
-					{
-						text: {name: "Author"}
-					}
-				]
-			},
-			{
-				text: { name: "Operation Manager" },
-				connectors: {
-					style: {
-						'stroke': '#bbb',
-						"stroke-dasharray": "- .", //"", "-", ".", "-.", "-..", ". ", "- ", "--", "- .", "--.", "--.."
-						'arrow-start': 'classic-wide-long'
-					}
-				},
-				children: [
-					{
-						text: {name: "Manager I"},
-						connectors: {
-							style: {
-								stroke: "#00CE67"
-							}
-						},
-						children: [
-							{
-								text: {name: "Worker I"}
-							},
-							{
-								pseudo: true,
-								connectors: {
-									style: {
-										stroke: "#00CE67"
-									}
-								},
-								children: [
-									{
-										text: {name: "Worker II"}
-									}
-								]
-							},
-							{
-								text: {name: "Worker III"}
-							}
-						]
-					},
-					{
-						text: {name: "Manager II"},
-						connectors: {
-							type: "curve",
-							style: {
-								stroke: "#50688D"
-							}
-						},
-						children: [
-							{
-								text: {name: "Worker I"}
-							},
-							{
-								text: {name: "Worker II"}
-							}
-						]
-					},
-					{
-						text: {name: "Manager III"},
-						connectors: {
-							style: {
-								'stroke': '#FF5555'
-							}
-						},
-						children: [
-							{
-								text: {name: "Worker I"}
-							},
-							{
-								pseudo: true,
-								connectors: {
-									style: {
-										'stroke': '#FF5555'
-									}
-								},
-								children: [
-									{
-										text: {name: "Worker II"}
-									},
-									{
-										text: {name: "Worker III"}
-									}
-								]
-							},
-							{
-								text: {name: "Worker IV"}
-							}
-						]
-					}
-				]
-			},
-			{
-				text: { name: "Delivery Manager" },
-				stackChildren: true,
-				connectors: {
-					stackIndent: 30,
-					style: {
-						'stroke': '#E3C61A',
-						'arrow-end': 'block-wide-long'
-					}
-				},
-				children: [
-					{
-						text: {name: "Driver I"}
-					},
-					{
-						text: {name: "Driver II"}
-					},
-					{
-						text: {name: "Driver III"}
-					}
-				]
-			}
-		]
-	}
-};
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+  
+  boxSelectionEnabled: false,
+  autounselectify: true,
+  zoomingEnabled:false,
+  style: [
+    {
+      selector: 'node',
+      css: {
+        'content': 'data(id)',
+        'text-valign': 'center',
+        'text-halign': 'center'
+      }
+    },
+    {
+      selector: '$node > node',
+      css: {
+        'padding-top': '10px',
+        'padding-left': '10px',
+        'padding-bottom': '10px',
+        'padding-right': '10px',
+        'text-valign': 'top',
+        'text-halign': 'center',
+        'background-color': '#bbb'
+      }
+    },
+    {
+      selector: 'edge',
+      css: {
+        'target-arrow-shape': 'triangle',
+        'curve-style': 'bezier'
+      }
+    },
+    {
+      selector: ':selected',
+      css: {
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      }
+    }
+  ],
+  
+  elements: {
+    nodes: [
+      { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
+      { data: { id: 'b' } },
+      { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
+      { data: { id: 'd' }, position: { x: 215, y: 175 } },
+      { data: { id: 'e' } },
+      { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+    ],
+    edges: [
+      { data: { id: 'ad', source: 'a', target: 'd' } },
+      { data: { id: 'eb', source: 'e', target: 'b' } }
+      
+    ]
+  },
+  
+  layout: {
+    name: 'preset',
+    padding: 5
+  }
+});
