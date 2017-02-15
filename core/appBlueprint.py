96c11bc8ae9038009adf7a5f34408346e753c217
egk865 <egk865@users.noreply.github.com>
2017-02-14 21:16:37 -0500

    Added several graphs and a map to HelloWorld app

--- core/appBlueprint.py
+++ core/appBlueprint.py
@@ -7,20 +7,19 @@ from flask_security import roles_required, auth_token_required
 from core import forms
 from core import interface
 
-appPage = Blueprint("appPage", "apps", template_folder=os.path.abspath('apps'), static_folder="resources")
+appPage = Blueprint("appPage", "apps", template_folder=os.path.abspath('apps'), static_folder="static")
 
 
 @appPage.url_value_preprocessor
 def staticRequestHandler(endpoint, values):
     g.app = values.pop('app', None)
-    appPage.static_folder = os.path.abspath('apps/' + g.app + '/interface/resources')
+    appPage.static_folder = os.path.abspath('apps/' + g.app + '/interface/static')
 
 @appPage.route('/display', methods=["POST"])
 @auth_token_required
 @roles_required("admin")
 def displayApp():
     form = forms.RenderArgsForm(request.form)
-
     path = g.app + "/interface/templates/" + form.page.data
     args = interface.loadApp(g.app, form.key.entries, form.value.entries)
 
