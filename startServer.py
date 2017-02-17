0c34dd24b83e2636133e2681fcaa67b758b40f61
Justin Tervala <Tervala_Justin@bah.com>
2017-02-17 10:44:44 -0500

    Server uses port fro core.config

--- startServer.py
+++ startServer.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     try:
         port = int(config.port)
         if ssl_context:
-            server = WSGIServer(('', 5000), application=flaskServer.app, ssl_context=ssl_context)
+            server = WSGIServer(('', port), application=flaskServer.app, ssl_context=ssl_context)
         else:
-            server = WSGIServer(('', 5000), application=flaskServer.app)
+            server = WSGIServer(('', port), application=flaskServer.app)
         server.serve_forever()
     except ValueError:
         print('Invalid port {0}. Port must be an integer'.format(config.port))
