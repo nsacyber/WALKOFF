43e4e7bebe727ff92b7bb987a00c0ed3d9c08107
Justin Tervala <Tervala_Justin@bah.com>
2017-02-27 12:18:20 -0500

    Added unit tests to HelloWorld app

--- apps/HelloWorld/main.py
+++ apps/HelloWorld/main.py
@@ -1,31 +1,32 @@
 from core import app
 
-#There is an associated Hello world test workflow which can be executed
+
+# There is an associated Hello world test workflow which can be executed
 
 class Main(app.App):
     def __init__(self, name=None, device=None):
-        #The parent app constructor looks for a device configuration and returns that as a dictionary called self.config
+        # The parent app constructor looks for a device configuration and returns that as a dictionary called self.config
         app.App.__init__(self, name, device)
-        #Functions and Variables that are designed to exist across functions go here
-        self.introMessage = {"message":"HELLO WORLD"}
+        # Functions and Variables that are designed to exist across functions go here
+        self.introMessage = {"message": "HELLO WORLD"}
 
-    #Every function in Main is an action that can be taken
-    #Every function needs to define an args argument which recieves a dictionary of input parameters
+    # Every function in Main is an action that can be taken
+    # Every function needs to define an args argument which recieves a dictionary of input parameters
     def helloWorld(self, args={}):
-        #LOOK AT YOUR CONSOLE WHEN EXECUTING
-        #print(self.introMessage)
+        # LOOK AT YOUR CONSOLE WHEN EXECUTING
+        # print(self.introMessage)
         return self.introMessage
 
-    #Example using arguments
-    #Repeats back the contents of the call argument
+    # Example using arguments
+    # Repeats back the contents of the call argument
     def repeatBackToMe(self, args={}):
-        #print("REPEATING: " + args["call"]())
+        # print("REPEATING: " + args["call"]())
         return "REPEATING: " + args["call"]()
 
-    #Increments number by one
+    # Increments number by one
     def returnPlusOne(self, args={}):
         return str(int(args["number"]()) + 1)
 
     def shutdown(self):
-        #print("SHUTTING DOWN")
+        # print("SHUTTING DOWN")
         return
