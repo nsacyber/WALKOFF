83a77a1235cfc6f4cd5181eb5407007a626d711c
Justin Tervala <Tervala_Justin@bah.com>
2017-02-27 10:27:10 -0500

    Split up unittests into suites; reduced verbosity of output

--- tests/testCaseSubscriptions.py
+++ tests/testCaseSubscriptions.py
@@ -5,15 +5,8 @@ from core.case.subscription import _SubscriptionEventList
 from tests.util.case import *
 
 
-class TestCaseSubscriptions(unittest.TestCase):
-    def setUp(self):
-        case_database.initialize()
-
-    def tearDown(self):
-        case_database.case_db.tearDown()
-
-    def test_subscription_list(self):
-
+class TestCaseSubscriptionList(unittest.TestCase):
+    def test_subscription_list_star_init(self):
         subscription_list1 = _SubscriptionEventList.construct('*')
         self.assertIsNotNone(subscription_list1, '_SubscriptionList constructed with "*" is None')
         self.assertTrue(subscription_list1.all,
@@ -21,6 +14,7 @@ class TestCaseSubscriptions(unittest.TestCase):
         self.assertTrue(subscription_list1.is_subscribed('a'),
                         '_SubscriptionList constructed with "*" should be subscribed to all events')
 
+    def test_subscription_list_list_init(self):
         subscription_list2_events = ['a', 'b', 'c', 'd']
         subscription_list2 = _SubscriptionEventList.construct(subscription_list2_events)
         self.assertIsNotNone(subscription_list2, '_SubscriptionList constructed with a list of events is None')
@@ -30,6 +24,7 @@ class TestCaseSubscriptions(unittest.TestCase):
                         'Some events subscribed to in _SubscriptionList are improperly subscribed to')
         self.assertFalse(subscription_list2.is_subscribed('ff'), 'event not subscribed to is claiming to be subscribed')
 
+    def test_subscription_list_single_init(self):
         subscription_list3 = _SubscriptionEventList.construct('a')
         self.assertIsNotNone(subscription_list3, '_SubscriptionList constructed with a single events is None')
         self.assertFalse(subscription_list3.all,
@@ -38,113 +33,145 @@ class TestCaseSubscriptions(unittest.TestCase):
                         'Some events subscribed to in _SubscriptionList are improperly subscribed to')
         self.assertFalse(subscription_list3.is_subscribed('ff'), 'event not subscribed to is claiming to be subscribed')
 
-    def test_global_subscriptions(self):
-        step_subs_events = ['b', 'c']
-        filter_subs_events = ['d', 'e']
-        global_subs = GlobalSubscriptions(controller='a', step=step_subs_events, flag='*', filter=filter_subs_events)
 
-        none_error_message = 'A subscription level which should not be None is None: {0}'
-        should_be_subscribed_message = 'A subscription level which should be subscribed ' \
+class TestGlobalSubscriptions(unittest.TestCase):
+    def setUp(self):
+        self.step_subs_events = ['b', 'c']
+        self.filter_subs_events = ['d', 'e']
+        self.global_subs = GlobalSubscriptions(controller='a', step=self.step_subs_events, flag='*',
+                                               filter=self.filter_subs_events)
+        self.none_error_message = 'A subscription level which should not be None is None: {0}'
+        self.should_be_subscribed_message = 'A subscription level which should be subscribed ' \
                                        'to events {0} is not subscribed: {1}'
-        should_not_be_subscribed_message = 'A subscription level which should be not subscribed ' \
+        self.should_not_be_subscribed_message = 'A subscription level which should be not subscribed ' \
                                            'to events {0} is subscribed: {1}'
-        unitialized_subscribed_message = 'An uninitialized subscription level should be subscriped to no events'
+        self.uninitialized_subscribed_message = 'An uninitialized subscription level should be subscribed to no events'
+        self.level_iter = iter(self.global_subs)
 
-        level_iter = iter(global_subs)
+    def test_iteration(self):
+        for _ in range(6):
+            next(self.level_iter)
+        with self.assertRaises(StopIteration):
+            next(self.level_iter)
 
-        controller_subs = next(level_iter)
-        self.assertIsNotNone(controller_subs, none_error_message.format('controller'))
-        self.assertTrue(controller_subs.is_subscribed('a'), should_be_subscribed_message.format('a', 'controller'))
-        self.assertFalse(controller_subs.is_subscribed('b'), should_not_be_subscribed_message.format('b', 'controller'))
+    def test_controller_subscriptions(self):
+        controller_subs = next(self.level_iter)
+        self.assertIsNotNone(controller_subs, self.none_error_message.format('controller'))
+        self.assertTrue(controller_subs.is_subscribed('a'), self.should_be_subscribed_message.format('a', 'controller'))
+        self.assertFalse(controller_subs.is_subscribed('b'), self.should_not_be_subscribed_message.format('b', 'controller'))
 
-        workflow_subs = next(level_iter)
-        self.assertIsNotNone(workflow_subs, none_error_message.format('workflow'))
-        self.assertFalse(workflow_subs.is_subscribed('b'), unitialized_subscribed_message)
+    def test_workflow_subscriptions(self):
+        next(self.level_iter)
+        workflow_subs = next(self.level_iter)
+        self.assertIsNotNone(workflow_subs, self.none_error_message.format('workflow'))
+        self.assertFalse(workflow_subs.is_subscribed('b'), self.uninitialized_subscribed_message)
 
-        step_subs = next(level_iter)
-        self.assertIsNotNone(step_subs, none_error_message.format('step'))
-        self.assertTrue(all(step_subs.is_subscribed(event_name) for event_name in step_subs_events),
-                        should_be_subscribed_message.format(step_subs_events, 'step'))
-        self.assertFalse(step_subs.is_subscribed('a'), should_not_be_subscribed_message.format('a', 'step'))
+    def test_step_subscriptions(self):
+        next(self.level_iter)
+        next(self.level_iter)
+        step_subs = next(self.level_iter)
+        self.assertIsNotNone(step_subs, self.none_error_message.format('step'))
+        self.assertTrue(all(step_subs.is_subscribed(event_name) for event_name in self.step_subs_events),
+                        self.should_be_subscribed_message.format(self.step_subs_events, 'step'))
+        self.assertFalse(step_subs.is_subscribed('a'), self.should_not_be_subscribed_message.format('a', 'step'))
 
-        nextstep_subs = next(level_iter)
-        self.assertIsNotNone(nextstep_subs, none_error_message.format('next step'))
-        self.assertFalse(nextstep_subs.is_subscribed('b'), unitialized_subscribed_message)
+    def test_nextstep_subscriptions(self):
+        for _ in range(3):
+            next(self.level_iter)
+        nextstep_subs = next(self.level_iter)
+        self.assertIsNotNone(nextstep_subs, self.none_error_message.format('next step'))
+        self.assertFalse(nextstep_subs.is_subscribed('b'), self.uninitialized_subscribed_message)
 
-        flag_subs = next(level_iter)
-        self.assertIsNotNone(flag_subs, none_error_message.format('flag'))
+    def test_flag_subscriptions(self):
+        for _ in range(4):
+            next(self.level_iter)
+        flag_subs = next(self.level_iter)
+        self.assertIsNotNone(flag_subs, self.none_error_message.format('flag'))
         self.assertTrue(all(flag_subs.is_subscribed(event_name) for event_name in ['a', 'b', 'ff']),
                         'subscription level initialized with "*" should be subscribed to all events')
 
-        filter_subs = next(level_iter)
-        self.assertIsNotNone(filter_subs, none_error_message.format('filter'))
-        self.assertTrue(all(filter_subs.is_subscribed(event_name) for event_name in filter_subs_events),
-                        should_be_subscribed_message.format(filter_subs_events, 'filter'))
-        self.assertFalse(filter_subs.is_subscribed('a'), should_not_be_subscribed_message.format('a', 'filter'))
+    def test_filter_subscriptions(self):
+        for _ in range(5):
+            next(self.level_iter)
+        filter_subs = next(self.level_iter)
+        self.assertIsNotNone(filter_subs, self.none_error_message.format('filter'))
+        self.assertTrue(all(filter_subs.is_subscribed(event_name) for event_name in self.filter_subs_events),
+                        self.should_be_subscribed_message.format(self.filter_subs_events, 'filter'))
+        self.assertFalse(filter_subs.is_subscribed('a'), self.should_not_be_subscribed_message.format('a', 'filter'))
 
-        with self.assertRaises(StopIteration):
-            next(level_iter)
+class TestSubscription(unittest.TestCase):
+    def setUp(self):
+        self.sub1_events = ['a', 'b', 'c']
+        self.global_sub1_events = ['a', 'd', 'f', 'g']
+        self.global_sub1 = _SubscriptionEventList(self.global_sub1_events)
+        self.sub1_all_events = set(self.sub1_events) | set(self.global_sub1_events)
+        self.sub2_disabled = ['a', 'd', 'f']
+        self.valid_with_global = set(self.sub1_all_events) - set(self.sub2_disabled)
 
-    def test_subscription(self):
-        sub1_events = ['a', 'b', 'c']
-        sub1 = Subscription(events=sub1_events)
-        global_sub1_events = ['a', 'd', 'f', 'g']
-        sub1_all_events = set(sub1_events) | set(global_sub1_events)
-        global_sub1 = _SubscriptionEventList(global_sub1_events)
+
+    def test_subscription_base(self):
+        sub1 = Subscription(events=self.sub1_events)
         self.assertIsNotNone(sub1, 'Subscription initialized with event=list should not be None')
-        self.assertTrue(all(sub1.is_subscribed(event_name) for event_name in sub1_events),
-                        'Subscription which should be subscribed to events {0} is not'.format(sub1_events))
-        self.assertTrue(all(sub1.is_subscribed(event_name, global_subs=global_sub1) for event_name in sub1_all_events),
+        self.assertTrue(all(sub1.is_subscribed(event_name) for event_name in self.sub1_events),
+                        'Subscription which should be subscribed to events {0} is not'.format(self.sub1_events))
+        self.assertTrue(all(sub1.is_subscribed(event_name, global_subs=self.global_sub1) for event_name in self.sub1_all_events),
                         'Subscription of {0} given global subscriptions {1} and is not subscribed '
-                        'some of events in {2}'.format(sub1_events, global_sub1_events, sub1_all_events))
+                        'some of events in {2}'.format(self.sub1_events, self.global_sub1_events, self.sub1_all_events))
         not_subscribed = ['x', 'y', 'z']
-        self.assertTrue(not any(sub1.is_subscribed(event_name, global_subs=global_sub1)
+        self.assertTrue(not any(sub1.is_subscribed(event_name, global_subs=self.global_sub1)
                                 for event_name in not_subscribed),
                         'Subscription of {0}, given global subscriptions {1}, is subscribed to'
-                        'some of events in {2}'.format(sub1_events, global_sub1_events, not_subscribed))
+                        'some of events in {2}'.format(self.sub1_events, self.global_sub1_events, not_subscribed))
 
-        sub2_disabled = ['a', 'd', 'f']
-        sub2 = Subscription(events=sub1_events, disabled=sub2_disabled)
+    def test_subscription_with_disabled(self):
+        sub2 = Subscription(events=self.sub1_events, disabled=self.sub2_disabled)
         self.assertIsNotNone(sub2, 'Subscription initialized with event=list and disabled=list should not be None')
-        self.assertTrue(not any(sub2.is_subscribed(event_name) for event_name in sub2_disabled),
+        self.assertTrue(not any(sub2.is_subscribed(event_name) for event_name in self.sub2_disabled),
                         'Subscription should not be subscribed to events which are disabled')
-        valid_subs = set(sub1_events) - set(sub2_disabled)
+        valid_subs = set(self.sub1_events) - set(self.sub2_disabled)
         self.assertTrue(all(sub2.is_subscribed(event_name) for event_name in valid_subs),
                         'Subscription should be subscribed to all events which are not disabled')
-        valid_with_global = set(sub1_all_events) - set(sub2_disabled)
-        self.assertTrue(all(sub2.is_subscribed(event_name, global_subs=global_sub1)
-                            for event_name in valid_with_global),
+        self.assertTrue(all(sub2.is_subscribed(event_name, global_subs=self.global_sub1)
+                            for event_name in self.valid_with_global),
                         'Subscription should be subscribed to all events in either self or global which '
                         'are not disabled')
-        self.assertTrue(not any(sub2.is_subscribed(event_name, global_subs=global_sub1)
-                                for event_name in sub2_disabled),
+        self.assertTrue(not any(sub2.is_subscribed(event_name, global_subs=self.global_sub1)
+                                for event_name in self.sub2_disabled),
                         'Subscription should not be subscribed to any events which are disabled')
 
-        sub3 = Subscription(events=sub1_events, disabled='*')
+    def test_subscription_with_all_disabled(self):
+        sub3 = Subscription(events=self.sub1_events, disabled='*')
         self.assertIsNotNone(sub3, 'Subscription initialized with event=list and disabled="*" should not be None')
-        self.assertTrue(not any(sub3.is_subscribed(event_name) for event_name in sub1_events),
+        self.assertTrue(not any(sub3.is_subscribed(event_name) for event_name in self.sub1_events),
                         'Subscription should not be subscribed to any events when all are disabled')
-        valid_with_global = set(sub1_all_events) - set(sub2_disabled)
-        self.assertTrue(not any(sub3.is_subscribed(event_name, global_subs=global_sub1)
-                                for event_name in valid_with_global),
+        self.assertTrue(not any(sub3.is_subscribed(event_name, global_subs=self.global_sub1)
+                                for event_name in self.valid_with_global),
                         'Subscription should not be subscribed to any events in either self or global when all are '
                         'not disabled')
-        self.assertTrue(not any(sub3.is_subscribed(event_name, global_subs=global_sub1)
-                                for event_name in sub2_disabled),
+        self.assertTrue(not any(sub3.is_subscribed(event_name, global_subs=self.global_sub1)
+                                for event_name in self.sub2_disabled),
                         'Subscription should not be subscribed to any events which are disabled')
 
+    def test_subscribed_with_all_enabled_all_disabled(self):
         sub4 = Subscription(events='*', disabled='*')
         self.assertIsNotNone(sub4, 'Subscription initialized with event="*" and disabled="*" should not be None')
-        self.assertTrue(not any(sub4.is_subscribed(event_name) for event_name in sub1_events),
+        self.assertTrue(not any(sub4.is_subscribed(event_name) for event_name in self.sub1_events),
                         'Subscription should not be subscribed to any events when all are disabled')
-        valid_with_global = set(sub1_all_events) - set(sub2_disabled)
-        self.assertTrue(not any(sub4.is_subscribed(event_name, global_subs=global_sub1)
-                                for event_name in valid_with_global),
+        self.assertTrue(not any(sub4.is_subscribed(event_name, global_subs=self.global_sub1)
+                                for event_name in self.valid_with_global),
                         'Subscription should not be subscribed to any events in either self or global when all are '
                         'not disabled')
-        self.assertTrue(not any(sub4.is_subscribed(event_name, global_subs=global_sub1)
-                                for event_name in sub2_disabled),
+        self.assertTrue(not any(sub4.is_subscribed(event_name, global_subs=self.global_sub1)
+                                for event_name in self.sub2_disabled),
                         'Subscription should not be subscribed to any events which are disabled')
+
+
+class TestCaseSubscriptions(unittest.TestCase):
+    def setUp(self):
+        case_database.initialize()
+
+    def tearDown(self):
+        case_database.case_db.tearDown()
 
     def test_case_subscriptions(self):
         case, acceptance = construct_case1()
