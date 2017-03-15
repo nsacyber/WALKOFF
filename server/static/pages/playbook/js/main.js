809455cae0dfb32c2d38e6505a4084560d7d7024
egk865 <egk865@users.noreply.github.com>
2017-03-15 11:57:50 -0400

    created new workflow tab in playbook page for loading workflows in the graph by double clicking on a workflow in the list

--- server/static/pages/playbook/js/main.js
+++ server/static/pages/playbook/js/main.js
@@ -3,7 +3,126 @@ $(function(){
         $(this).tab('show');
     });
 
+    //-------------------
+    // Setup Actions tree
+    //-------------------
+
+    // This is called while the user is dragging
+    function dragHelper( event ) {
+        // Return empty div for helper so that original dragged item does not move
+        return '<div></div>';
+    }
+
+    // Reformat the JSON data returned from the /apps/actions endpoint
+    // into a format that jsTree can understand.
+    function formatAppsActionJsonDataForJsTree(data) {
+        data = JSON.parse(data);
+        var jstreeData = [];
+        $.each(data, function( key, value ) {
+            var appName = key;
+            var actionNames = data[key];
+            var app = {};
+            app.text = key;
+            app.children = [];
+            for (var i=0; i<actionNames.length; ++i) {
+                app.children.push({text: actionNames[i], icon: "jstree-file", data: {app: appName}});
+            }
+            jstreeData.push(app);
+        });
+        return jstreeData;
+    }
+
+    // Download all actions in all apps for display in the Actions tree
+    $.ajax({
+        'async': true,
+        'type': "GET",
+        'global': false,
+        'headers':{"Authentication-Token":authKey},
+        'url': "/apps/actions",
+        'success': function (data) {
+            $('#actions').jstree({
+                'core' : {
+                    'data' : formatAppsActionJsonDataForJsTree(data)
+                }
+            })
+            .bind("ready.jstree", function (event, data) {
+                $(this).jstree("open_all"); // Expand all
+
+                // Make each leaf node draggable onto the graph
+                $('.jstree-leaf').each(function(){
+                    $(this).draggable( {
+                        cursor: 'copy',
+                        cursorAt: { left: 0, top: 0 },
+                        containment: 'document',
+                        helper: dragHelper
+                    });
+                });
+            }).
+                on('after_open.jstree', function (e, data) {
+                    console.log(e);
+                    console.log(data);
+                    //var i, j, r = [];
+                    //for(i = 0, j = data.selected.length; i < j; i++) {
+                    //    r.push(data.instance.get_node(data.selected[i]).text);
+                    //}
+                    //$('#event_result').html('Selected: ' + r.join(', '));
+                });
+        }
+    });
+
+    //--------------------
+    // Setup Workflow list
+    //--------------------
+
+    // Reformat the JSON data returned from the /workflows endpoint
+    // into a format that jsTree can understand.
+    function formatWorkflowJsonDataForJsTree(data) {
+        data = JSON.parse(data).workflows.sort();
+        var jstreeData = [];
+        $.each(data, function( index, value ) {
+            var workflowName = value;
+            var app = {};
+            app.text = workflowName;
+            app.icon = "jstree-file";
+            jstreeData.push(app);
+        });
+        return jstreeData;
+    }
+
+    // Download list of workflows for display in the Workflows list
+    $.ajax({
+        'async': true,
+        'type': "GET",
+        'global': false,
+        'headers':{"Authentication-Token":authKey},
+        'url': "/workflows",
+        'success': function (data) {
+            $('#workflows').jstree({
+                'core' : {
+                "check_callback" : true,
+                    'multiple': false, // Disable multiple selection
+                    'data' : formatWorkflowJsonDataForJsTree(data)
+                },
+                "plugins" : [ "contextmenu", "wholerow" ] // Show workflows as a list
+                                           // of rows, not in tree form
+            });
+            // handle double click on workflow
+            $("#workflows").bind("dblclick.jstree", function (event, data) {
+
+                var node = $(event.target).closest("li");
+                var node_id = node[0].id; //id of the selected node
+                node = $('#workflows').jstree(true).get_node(node_id);
+
+                var workflowName = node.text;
+
+                // do something
+                loadWorkflow(workflowName);
+            });
+        }
+    });
+
 });
+
 
 $("#executeWorkflowButton").on("click", function(e){
     var result = function () {
@@ -24,22 +143,6 @@ $("#executeWorkflowButton").on("click", function(e){
     notifyMe();
 })
 
-if(currentWorkflow){
-    var workflowData = function () {
-        var tmp = null;
-        $.ajax({
-            'async': false,
-            'type': "POST",
-            'global': false,
-            'headers':{"Authentication-Token":authKey},
-            'url': "/workflow/" + currentWorkflow + "/cytoscape",
-            'success': function (data) {
-                tmp = data;
-            }
-        });
-        return tmp;
-    }();
-}
 
 function notifyMe() {
   if (!Notification) {
@@ -62,16 +165,71 @@ function notifyMe() {
   }
 }
 
-console.log(workflowData);
+
+var cy = null;
 
 
-$(function(){
+function newWorkflow(workflowName) {
+    $.ajax({
+        'async': false,
+        'type': "POST",
+        'global': false,
+        'headers':{"Authentication-Token":authKey},
+        'url': "/workflow/" + workflowName + "/add",
+        'success': function (data) {
+            loadWorkflow("Workflow1");
+        }
+    });
+}
+
+
+function saveWorkflow(workflowName) {
+    if (cy !== null) {
+        var workflowData = {filename: "", cytoscape: cy.elements().jsons()};
+        $.ajax({
+            'async': false,
+            'type': "POST",
+            'global': false,
+            'headers':{"Authentication-Token":authKey},
+            'url': "/workflow/" + workflowName + "/save",
+            'data': workflowData,
+            'success': function (data) {
+            }
+        });
+    }
+}
+
+
+function loadWorkflow(workflowName) {
+    saveWorkflow(workflowName);
+
+    var currentWorkflow = workflowName;
+    $("#currentWorkflowText").text(currentWorkflow);
+
+    if(currentWorkflow){
+        var workflowData = function () {
+            var tmp = null;
+            $.ajax({
+                'async': false,
+                'type': "POST",
+                'global': false,
+                'headers':{"Authentication-Token":authKey},
+                'url': "/workflow/" + currentWorkflow + "/cytoscape",
+                'success': function (data) {
+                    tmp = data;
+                }
+            });
+            return tmp;
+        }();
+    }
+
+    console.log(workflowData);
 
   //---------------------------
   // Create the Cytoscape graph
   //---------------------------
 
-  var cy = cytoscape({
+    cy = cytoscape({
     container: document.getElementById('cy'),
 
     boxSelectionEnabled: false,
@@ -168,6 +326,41 @@ $(function(){
   // Define various helper functions
   //--------------------------------
 
+    function onClickNew(e) {
+        var ele = e.cyTarget;
+        var parameters = ele.data().parameters;
+        var data = [
+            {
+                text: "Id: " + ele.data().id
+            },
+            {
+                text: "Name: " + parameters.name
+            },
+            {
+                text: "Action: " + parameters.app + "/" + parameters.action
+            },
+            {
+                text: "Device: " + parameters.device
+            },
+            {
+                text: "Input: " + JSON.stringify(parameters.input)
+            },
+            {
+                text: "Errors: " + JSON.stringify(parameters.errors)
+            },
+
+        ];
+        $('#parameters').jstree({
+            'core' : {
+                "animation" : 0,
+                'force_text' : true,
+                'data' : data,
+                "check_callback" : true
+            },
+            "plugins" : [ "wholerow", "state", "types", "contextmenu" ]
+        });
+    }
+
   // This function displays info about a node/edge when clicked upon next to the graph
   function onClick(e) {
 
@@ -183,12 +376,6 @@ $(function(){
     var parameters = ele.data().parameters;
     var parametersAsJsonString = jsonStringifySort(parameters);
     $("#parameters").text(parametersAsJsonString);
-  }
-
-  // This is called while the user is dragging
-  function dragHelper( event ) {
-    // Return empty div for helper so that original dragged item does not move
-    return '<div></div>';
   }
 
   // This function is called when the user drops a new node onto the graph
@@ -293,6 +480,15 @@ $(function(){
     ur.redo();
   });
 
+  // Handle new button press
+  $( "#new-button" ).click(function() {
+  });
+
+  // Handle save button press
+  $( "#save-button" ).click(function() {
+      saveWorkflow(currentWorkflow);
+  });
+
   // Handle delete button press
   $( "#remove-button" ).click(function() {
     removeSelectedNodes();
@@ -342,52 +538,4 @@ $(function(){
       }
   });
 
-    // Reformat the JSON data returned from the /apps/actions endpoint
-    // into a format that jsTree can understand.
-    function formatAppsActionJsonDataForJsTree(data) {
-        data = JSON.parse(data);
-        var jstreeData = [];
-        $.each(data, function( key, value ) {
-            var appName = key;
-            var actionNames = data[key];
-            var app = {};
-            app.text = key;
-            app.children = [];
-            for (var i=0; i<actionNames.length; ++i) {
-                app.children.push({text: actionNames[i], data: {app: appName}});
-            }
-            jstreeData.push(app);
-        });
-        return jstreeData;
-    }
-
-    // Download all actions in all apps for display in the Actions tree
-    $.ajax({
-        'async': true,
-        'type': "GET",
-        'global': false,
-        'headers':{"Authentication-Token":authKey},
-        'url': "/apps/actions",
-        'success': function (data) {
-            $('#actions').jstree({
-                'core' : {
-                    'data' : formatAppsActionJsonDataForJsTree(data)
-                }
-            })
-            .bind("ready.jstree", function (event, data) {
-                $(this).jstree("open_all"); // Expand all
-
-                // Make each leaf node draggable onto the graph
-                $('.jstree-leaf').each(function(){
-                    $(this).draggable( {
-                        cursor: 'copy',
-                        cursorAt: { left: 0, top: 0 },
-                        containment: 'document',
-                        helper: dragHelper
-                    });
-                });
-            });
-        }
-    });
-
-});
+}
