5fec44ae75d663cbf4c31e9c1ba2487cee292bb0
Corey Robins <robins_corey@bah.com>
2017-05-02 10:06:33 -0400

    Added CyberAnalytic App

---
+++ apps/CyberAnalytic/main.py
@@ -0,0 +1,39 @@
+from server.appdevice import App
+import psutil
+import gevent
+import json
+from collections import namedtuple
+
+threat_pid = namedtuple("thread_pid", ["threat_name", "threat_exe", "pid"])
+suspicious_pids = []
+
+class Main(App):
+
+    global suspicious_pids
+
+    def __init__(self):
+        self.is_running = False
+
+    def __monitor_processes(self):
+        while True:
+            for proc in psutil.process_iter():
+                if 'at.exe' in proc.exe():
+                    suspicious_pids.append(threat_pid("at.exe", proc.name(), proc.pid))
+                elif 'schtask.exe' in proc.exe():
+                    suspicious_pids.append(threat_pid("schtask.exe", proc.name(), proc.pid))
+                elif 'cmd.exe' in proc.exe():
+                    if proc.parent() and "explorer.exe" not in proc.parent().exe():
+                        suspicious_pids.append(threat_pid("cmd.exe", proc.name(), proc.pid))
+
+    def begin_monitoring(self, args={}):
+        if not self.is_running:
+            gevent.spawn(self.__monitor_processes)
+
+    def get_exe_pids(self, args={}):
+        return json.dumps(suspicious_pids)
+
+    def get_exe_pids_by_name(self, args={}):
+        if args["name"]:
+            return [ x for x in suspicious_pids if x.name == args["name"] ]
+        else:
+            return []
