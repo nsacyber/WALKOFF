41581183544ca0f590c96eac6d625ff92866ca5d
Corey Robins <robins_corey@bah.com>
2017-05-04 14:45:32 -0400

    Added documentation to context module

--- server/context.py
+++ server/context.py
@@ -3,10 +3,10 @@ import core.config.paths
 
 
 class Context(object):
-    """
-    Act as an interface for objects to access other event specific variables that might be needed
-    """
     def __init__(self):
+        """Initializes a new Context object. This acts as an interface for objects to access other event specific
+            variables that might be needed.
+        """
         self.apps = self.get_apps()
 
         from server.app import app
@@ -28,14 +28,24 @@ class Context(object):
         self.user_datastore = user_datastore
         self.controller = controller
 
-    # Returns list of apps
-    # Gets all the app instances
     @staticmethod
     def get_apps(path=core.config.paths.apps_path):
+        """Gets all the App instances.
+        Args:
+            path (str, optional): The path to the apps. Defaults to the apps_path set in the configuration.
+        Returns:
+            A list of App instances.
+        """
         return list_apps(path=path)
 
     @staticmethod
     def get_widgets(path=core.config.paths.apps_path):
+        """Gets a dictionary of all Widgets for every App.
+        Args:
+            path (str, optional): The path to the apps. Defaults to the apps_path set in the configuration.
+        Returns:
+            A dictionary containing all App objects and any Widgets associated with them.
+        """
         ret = []
         for app in Context.get_apps():
             for widget in list_widgets(app, path):
@@ -43,15 +53,24 @@ class Context(object):
         return ret
 
     def set(self, key, value):
+        """Sets an attribute for the object.
+        Args:
+            key (str): The name of the attribute to set.
+            value (any): The value of the attribute to set.
+        """
         setattr(self, key, value)
 
     @staticmethod
     def init_threads():
+        """Initializes the thread pool.
+        """
         from core.controller import initialize_threading
         initialize_threading()
 
     @staticmethod
     def shutdown_threads():
+        """Shuts down the threadpool.
+        """
         from core.controller import shutdown_pool
         shutdown_pool()
 
