fa0eb3db2964ccf09e1c733a5e314e4d9af16d3b
Justin Tervala <Tervala_Justin@bah.com>
2017-05-04 14:19:35 -0400

    Sphinx documentation for some core modules

--- core/case/database.py
+++ core/case/database.py
@@ -17,6 +17,9 @@ class _CaseEventLink(_Base):
 
 
 class Case(_Base):
+    """
+    Case ORM for the events database
+    """
     __tablename__ = 'case'
     id = Column(Integer, primary_key=True)
     name = Column(String)
@@ -24,6 +27,13 @@ class Case(_Base):
     events = relationship('Event', secondary='case_event', lazy='dynamic')
 
     def as_json(self, with_events=True):
+        """Gets the JSON representation of a Case object.
+        Args:
+            with_events (bool, optional): A boolean to determine whether or not the events of the Case object should be
+                included in the output.
+        Returns:
+            The JSON representation of a Case object.
+        """
         output = {'id': str(self.id),
                   'name': self.name,
                   'note': self.note}
@@ -33,6 +43,9 @@ class Case(_Base):
 
 
 class Event(_Base):
+    """
+    ORM for an Event in the events database
+    """
     __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
     timestamp = Column(DateTime, default=datetime.utcnow())
@@ -44,6 +57,13 @@ class Event(_Base):
     cases = relationship('Case', secondary='case_event', lazy='dynamic')
 
     def as_json(self, with_cases=False):
+        """Gets the JSON representation of an Event object.
+        Args:
+            with_cases (bool, optional): A boolean to determine whether or not the cases of the event object should be
+                included in the output.
+        Returns:
+            The JSON representation of an Event object.
+        """
         output = {'id': str(self.id),
                   'timestamp': str(self.timestamp),
                   'type': self.type,
@@ -64,6 +84,17 @@ class Event(_Base):
 
     @staticmethod
     def create(sender, timestamp, entry_message, entry_type, data=''):
+        """Factory method to construct an Event object.
+        Args:
+            sender (cls): A boolean to determine whether or not the events of the Case object should be
+                included in the output.
+            timestamp (str): A string representation of a timestamp
+            entry_message (str): The message associated with the event
+            entry_type (str): The type of event being logged (Workflow, NextStep, Flag, etc.)
+            data (str): Extra information to be logged with the event
+        Returns:
+            An Event object.
+        """
         return Event(type=entry_type,
                      timestamp=timestamp,
                      ancestry=','.join(map(str, sender.ancestry)),
@@ -72,10 +103,15 @@ class Event(_Base):
 
 
 class CaseDatabase(object):
+    """
+    Wrapper for the SQLAlchhemy Case database object
+    """
     def __init__(self):
         self.create()
 
     def create(self):
+        """ Creates the database
+        """
         self.engine = create_engine('sqlite:///' + case_db_path)
         self.connection = self.engine.connect()
         self.transaction = self.connection.begin()
@@ -88,16 +124,26 @@ class CaseDatabase(object):
         _Base.metadata.create_all(self.engine)
 
     def tear_down(self):
+        """ Tears down the database
+        """
         self.session.rollback()
         self.connection.close()
         self.engine.dispose()
 
-    def register_events(self, case_names):
+    def add_cases(self, case_names):
+        """ Adds empty cases to the database
+        Args:
+            case_names (list[str]): A list of case names to add
+        """
         additions = [Case(name=case_name) for case_name in set(case_names)]
         self.session.add_all(additions)
         self.session.commit()
 
     def delete_cases(self, case_names):
+        """ Removes cases to the database
+        Args:
+            case_names (list[str]): A list of case names to remove
+        """
         if case_names:
             cases = self.session.query(Case).filter(Case.name.in_(case_names)).all()
             for case in cases:
@@ -105,6 +151,11 @@ class CaseDatabase(object):
             self.session.commit()
 
     def rename_case(self, old_case_name, new_case_name):
+        """ Renames a case
+        Args:
+            old_case_name (str): The case to rename
+            new_case_name (str): The case's new name
+        """
         if old_case_name and new_case_name:
             case = self.session.query(Case).filter(Case.name == old_case_name).first()
             if case:
@@ -112,6 +163,11 @@ class CaseDatabase(object):
                 self.session.commit()
 
     def edit_case_note(self, case_name, note):
+        """ Edits the note attached to a case
+        Args:
+            case_name (str): The case to edit
+            note (str): The case's note
+        """
         if case_name:
             case = self.session.query(Case).filter(Case.name == case_name).first()
             if case:
@@ -119,6 +175,11 @@ class CaseDatabase(object):
                 self.session.commit()
 
     def edit_event_note(self, event_id, note):
+        """ Edits the note attached to an event
+        Args:
+            event_id (int): The id of the event
+            note (str): The event's note
+        """
         if event_id:
             event = self.session.query(Event).filter(Event.id == event_id).first()
             if event:
@@ -126,6 +187,11 @@ class CaseDatabase(object):
                 self.session.commit()
 
     def add_event(self, event, cases):
+        """ Adds an event to some cases
+        Args:
+            event (cls): A core.case.callbacks._EventEntry object to add to the cases
+            cases (list[str]): The cases to add the event to
+        """
         event_log = Event(type=event.type,
                           timestamp=event.timestamp,
                           ancestry=','.join(map(str, event.ancestry)),
@@ -144,21 +210,35 @@ class CaseDatabase(object):
         self.session.commit()
 
     def cases_as_json(self):
+        """Gets the JSON representation of all the cases in the case database.
+        Returns:
+            The JSON representation of all Case objects without their events.
+        """
         return {'cases': [case.as_json(with_events=False)
                           for case in self.session.query(Case).all()]}
 
     def event_as_json(self, event_id):
+        """Gets the JSON representation of an event in the case database.
+        Returns:
+            The JSON representation of an Event object.
+        """
         return self.session.query(Event).filter(Event.id == event_id).first().as_json()
 
     def case_events_as_json(self, case_name):
-        id = self.session.query(Case).filter(Case.name == case_name).first().id
-        if id:
-            result = [event.as_json() for event in self.session.query(Event).join(Event.cases).filter(Case.id==id).all()]
+        """Gets the JSON representation of all the events in the case database.
+        Returns:
+            The JSON representation of all Event objects without their cases.
+        """
+        event_id = self.session.query(Case).filter(Case.name == case_name).first().id
+        if event_id:
+            result = [event.as_json()
+                      for event in self.session.query(Event).join(Event.cases).filter(Case.id == event_id).all()]
             return result
         return {}
 
 
 def get_case_db(_singleton=CaseDatabase()):
+    """ Singleton factory which returns the case database"""
     return _singleton
 
 case_db = get_case_db()
@@ -166,12 +246,16 @@ case_db = get_case_db()
 
 # Initialize Module
 def initialize():
+    """ Initializes the case database
+    """
     _Base.metadata.drop_all()
     _Base.metadata.create_all()
 
 
 # Teardown Module
 def tear_down():
+    """ Tears down the case database
+    """
     case_db.session.close()
     case_db.transaction.rollback()
     case_db.connection.close()
--- core/case/subscription.py
+++ core/case/subscription.py
@@ -1,7 +1,7 @@
-# from core.context import running_context
 from core.case import database
 from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR, EVENT_JOB_ADDED, EVENT_JOB_REMOVED, \
     EVENT_SCHEDULER_START, EVENT_SCHEDULER_SHUTDOWN, EVENT_SCHEDULER_PAUSED, EVENT_SCHEDULER_RESUMED
+
 
 class GlobalSubscriptions(object):
     """
@@ -33,6 +33,10 @@ class GlobalSubscriptions(object):
         yield self.filter
 
     def as_json(self):
+        """Gets the JSON representation of all the GlobalSubscription object.
+        Returns:
+            The JSON representation of the GlobalSubscription object.
+        """
         return {"controller": self.controller,
                 "workflow": self.workflow,
                 "step": self.step,
@@ -41,14 +45,20 @@ class GlobalSubscriptions(object):
                 "filter": self.filter}
 
     @staticmethod
-    def from_json(json):
-        if set(json.keys()) == set(list(['controller', 'workflow', 'step', 'next_step', 'flag', 'filter'])):
-            return GlobalSubscriptions(controller=json['controller'],
-                                       workflow=json['workflow'],
-                                       step=json['step'],
-                                       next_step=json['next_step'],
-                                       flag=json['flag'],
-                                       filter=json['filter'])
+    def from_json(json_in):
+        """Forms a GlobalSubscription object from the provided JSON object.
+        Args:
+            json_in (JSON object): The JSON object to convert from.
+        Returns:
+            The GlobalSubscription object parsed from the JSON object.
+        """
+        if set(json_in.keys()) == set(list(['controller', 'workflow', 'step', 'next_step', 'flag', 'filter'])):
+            return GlobalSubscriptions(controller=json_in['controller'],
+                                       workflow=json_in['workflow'],
+                                       step=json_in['step'],
+                                       next_step=json_in['next_step'],
+                                       flag=json_in['flag'],
+                                       filter=json_in['filter'])
 
     def __repr__(self):
         return str({'controller': self.controller,
@@ -69,24 +79,38 @@ class Subscription(object):
     """
 
     def __init__(self, events=None, subscriptions=None):
+        """
+        Constructs a subscription object
+        Args:
+            events (list, optional): A list of events ID's to subscribe to. Usually strings or ints. Some can be found
+                in core.case.callbacks. Defaults to an empty list
+            subscriptions (dict{str: Subscription}, optional): The child nodes of the subscription object.
+                Takes the form of "subscription name => Subscription". Defaults to an empty dictionary
+        """
         self.events = events if events is not None else []
         self.subscriptions = subscriptions if subscriptions is not None else {}  # in form of {'name' => Subscription()}
 
     def is_subscribed(self, message_name):
         """
         Is the given message subscribed to in this level of execution?
-        :param message_name: The given message
-        :return (bool): Is the message subscribed to?
+        Args:
+             message_name (str): The given message
+        Returns:
+             Is the message subscribed to?
         """
         return message_name in self.events
 
-
-
     def as_json(self, names=False):
+        """Gets the JSON representation of all the subscription object.
+        Args:
+            names (bool, optional): Should the names of the controller events be converted to names? Defaults to False
+        Returns:
+            The JSON representation of the subscription object.
+        """
         if names:
             results = {"events": convert_to_event_names(self.events),
-                    "subscriptions": {str(name): subscription.as_json()
-                                      for name, subscription in self.subscriptions.items()}}
+                       "subscriptions": {str(name): subscription.as_json()
+                                         for name, subscription in self.subscriptions.items()}}
         else:
             results = {"events": self.events,
                        "subscriptions": {str(name): subscription.as_json()
@@ -95,6 +119,12 @@ class Subscription(object):
 
     @staticmethod
     def from_json(json_in):
+        """Forms a Subscription object from the provided JSON object.
+        Args:
+            json_in (JSON object): The JSON object to convert from.
+        Returns:
+            The Subscription object parsed from the JSON object.
+        """
         events = json_in['events'] if 'events' in json_in else None
         _subscriptions = json_in['subscriptions'] if 'subscriptions' in json_in else None
         if _subscriptions is not None:
@@ -107,11 +137,28 @@ class Subscription(object):
 
 
 class CaseSubscriptions(object):
+    """
+    An object encapsulating a case's subscriptions and its GlobalSubscriptions
+    """
+
     def __init__(self, subscriptions=None, global_subscriptions=None):
+        """ Constructs a case
+        Args:
+            subscriptions (dict{str: Subscription}, optional): The case's subscriptions. Keys should represent the
+                controller names. Defaults to an empty dictionary
+            global_subscriptions (GlobalSubscriptions, optional): The case's global subscriptions. Defaults to None
+        """
         self.subscriptions = subscriptions if subscriptions is not None else {}
         self.global_subscriptions = global_subscriptions if global_subscriptions is not None else GlobalSubscriptions()
 
     def is_subscribed(self, ancestry, message_name):
+        """ Checks if the case is subscribed to an event with the a given message and ancestry
+        Args:
+            ancestry (list[str]): The ancestry of teh sender of the event
+            message_name (str, int): The event type
+        Returns:
+             A boolean describing if the case is subscribed to the event or not
+        """
         current_subscriptions = self.subscriptions
         ancestry = list(ancestry[::-1])
         ancestry_level_name = ancestry.pop()
@@ -124,12 +171,22 @@ class CaseSubscriptions(object):
         return False
 
     def as_json(self):
+        """Gets the JSON representation of all the CaseSubscription object.
+        Returns:
+            The JSON representation of the CaseSubscription object.
+        """
         return {"subscriptions": {str(name): subscription.as_json()
                                   for name, subscription in self.subscriptions.items()},
                 "global_subscriptions": self.global_subscriptions.as_json()}
 
     @staticmethod
     def from_json(json_in):
+        """Forms a CaseSubscription object from the provided JSON object.
+        Args:
+            json_in (JSON object): The JSON object to convert from.
+        Returns:
+            The CaseSubscription object parsed from the JSON object.
+        """
         _subscriptions = json_in['subscriptions'] if 'subscriptions' in json_in else None
         if _subscriptions is not None:
             _subscriptions = {subscription_name: Subscription.from_json(subscription)
@@ -149,21 +206,35 @@ subscriptions = {}
 
 
 def set_subscriptions(new_subscriptions):
+    """ Resets the subscriptions
+    Args:
+        new_subscriptions (dict({str: CaseSubscriptions}): The new subscriptions.
+            Takes the form of "{subscription_name: CaseSubscriptions}"
+    """
     global subscriptions
     subscriptions = new_subscriptions
-    database.case_db.register_events(new_subscriptions.keys())
+    database.case_db.add_cases(new_subscriptions.keys())
 
 
 def add_cases(cases):
+    """ Adds the cases to the subscriptions
+    Args:
+        cases dict({str: CaseSubscription}): the cases and their associated subscriptions to add.
+            Takes the form of "{subscription_name: CaseSubscriptions}"
+    """
     valid_cases = []
     for case_name, case in cases.items():
         if case_name not in subscriptions:
             subscriptions[case_name] = case
             valid_cases.append(case_name)
-    database.case_db.register_events(valid_cases)
+    database.case_db.add_cases(valid_cases)
 
 
 def delete_cases(cases):
+    """ Deletes the cases from  the subscriptions
+    Args:
+        cases (list[str]): The names of teh cases to remove
+    """
     valid_cases = []
     for case_name in cases:
         if case_name in subscriptions:
@@ -173,29 +244,53 @@ def delete_cases(cases):
 
 
 def rename_case(old_case_name, new_case_name):
+    """ Renames a case
+    Args:
+        old_case_name (str): Case name to change
+        new_case_name (str): Case's new name
+    """
     if old_case_name in subscriptions:
         subscriptions[new_case_name] = subscriptions.pop(old_case_name)
         database.case_db.rename_case(old_case_name, new_case_name)
 
 
 def get_subscriptions():
+    """ Gets the subscriptions
+    """
     return subscriptions
 
 
 def clear_subscriptions():
+    """ Clears and resets the subscriptions
+    """
     global subscriptions
     subscriptions = {}
 
 
 def is_case_subscribed(case, ancestry, message_name):
+    """ Checks if a case is subscribed to a message given a message and its sender's ancestry
+    Args:
+        case (str): The name of the case
+        ancestry (list[str]): The ancestry of the sender
+        message_name (str): The name of the message to check
+    """
     return subscriptions[case].is_subscribed(ancestry, message_name)
 
 
 def subscriptions_as_json():
+    """ Gets a JSON representation of all the cases and case subscriptions
+    """
     return {str(name): subscription.as_json() for name, subscription in subscriptions.items()}
 
 
 def edit_global_subscription(case_name, global_subscriptions):
+    """ Edits a case's GlobalSubscription
+    Args:
+        case_name (str): The name of the case to edit
+        global_subscriptions (GlobalSubscriptions): The case's new gloabl subscriptions
+    Returns:
+        True if successfully edited. False otherwise.
+    """
     if case_name in subscriptions:
         subscriptions[case_name].global_subscriptions = global_subscriptions
         return True
@@ -203,6 +298,14 @@ def edit_global_subscription(case_name, global_subscriptions):
 
 
 def edit_subscription(case, ancestry, events):
+    """ Edits a subscription by changing the events to which a particular ancestry is subscribed to
+    Args:
+        case (str): The name of the case to edit
+        ancestry (list[str]): The ancestry to edit
+        events (list[str,int]): The ancestry's new events to which it is subscribed to
+    Returns:
+        True if successfully edited. False otherwise.
+    """
     if case in subscriptions:
         a = list(ancestry[::-1])
         current_subscriptions = subscriptions[case].subscriptions
@@ -236,6 +339,13 @@ def __construct_subscription_from_ancestry(ancestry, events):
 
 
 def add_subscription(case, ancestry, events):
+    """
+    Adds a subscription of a set events and ancestry to a case
+    Args:
+        case (str): The case to add a subscription to
+        ancestry (list[str]): The ancestry to add events to
+        events (list[str,int]): The events to subscribe to
+    """
     if case in subscriptions:
         ancestry = list(ancestry[::-1])
         current_subscriptions = subscriptions[case].subscriptions
@@ -248,8 +358,8 @@ def add_subscription(case, ancestry, events):
                     break
                 elif ancestry_level_name not in current_subscriptions:
                     ancestry.append(ancestry_level_name)
-                    current_subscriptions[ancestry_level_name] = __construct_subscription_from_ancestry(ancestry, events)[
-                        ancestry_level_name]
+                    current_subscriptions[ancestry_level_name] = \
+                        __construct_subscription_from_ancestry(ancestry, events)[ancestry_level_name]
                     break
                 else:
                     current_subscriptions = current_subscriptions[ancestry_level_name].subscriptions
@@ -260,6 +370,12 @@ def add_subscription(case, ancestry, events):
 
 
 def remove_subscription_node(case, ancestry):
+    """
+    Remove a case's subscription to an ancestry
+    Args:
+        case (str): The case to remove a subscription from
+        ancestry (list[str]): The ancestry to remove from the case's subscriptions
+    """
     if case in subscriptions:
         ancestry = list(ancestry[::-1])
         current_subscriptions = subscriptions[case].subscriptions
@@ -274,6 +390,7 @@ def remove_subscription_node(case, ancestry):
                 current_subscriptions = current_subscriptions[ancestry_level_name].subscriptions
                 ancestry_level_name = ancestry.pop()
 
+
 __scheduler_event_conversion = {'Scheduler Start': EVENT_SCHEDULER_START,
                                 'Scheduler Shutdown': EVENT_SCHEDULER_SHUTDOWN,
                                 'Scheduler Paused': EVENT_SCHEDULER_PAUSED,
@@ -283,7 +400,15 @@ __scheduler_event_conversion = {'Scheduler Start': EVENT_SCHEDULER_START,
                                 'Job Executed': EVENT_JOB_EXECUTED,
                                 'Job Error': EVENT_JOB_ERROR}
 
+
 def convert_to_event_names(events):
+    """
+    Converts events to controller event names if event is a controller event
+    Args:
+        events (list[str, int]): Events to be converted
+    Returns:
+        List of event identifiers in which the controller events have been converted to their string representations
+    """
     result = []
     for event in events:
         try:
