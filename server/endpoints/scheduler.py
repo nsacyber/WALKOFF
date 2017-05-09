af18c2a598392fd27c6e1b7ec6fd1c237fd878a9
Corey Robins <robins_corey@bah.com>
2017-05-09 11:24:02 -0400

    Edits to yamls and function defs

--- server/endpoints/scheduler.py
+++ server/endpoints/scheduler.py
@@ -1,54 +1,65 @@
 import json
-from flask import Blueprint, current_app
-from flask_security import auth_token_required, roles_accepted
-from server.flaskserver import running_context
-
-scheduler_page = Blueprint('scheduler_page', __name__)
+from flask import current_app
+from flask_security import roles_accepted
 
 
-@scheduler_page.route('/<string:action>', methods=['POST'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/scheduler'])
-def scheduler_actions(action):
-    if action == 'start':
+def start_scheduler():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         status = running_context.controller.start()
         current_app.logger.info('Scheduler started. Status {0}'.format(status))
         return json.dumps({"status": status})
-    elif action == 'stop':
+    return __func()
+
+def stop_scheduler():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         status = running_context.controller.stop()
         current_app.logger.info('Scheduler stopped. Status {0}'.format(status))
         return json.dumps({"status": status})
-    elif action == 'pause':
+    return __func()
+
+def pause_scheduler():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         status = running_context.controller.pause()
         current_app.logger.info('Scheduler paused. Status {0}'.format(status))
         return json.dumps({"status": status})
-    elif action == 'resume':
+    return __func()
+
+def resume_scheduler():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         status = running_context.controller.resume()
         current_app.logger.info('Scheduler resumed. Status {0}'.format(status))
         return json.dumps({"status": status})
-    current_app.logger.error('Scheduler cannot take action {0}. No such action'.format(action))
-    return json.dumps({"status": "invalid command"})
+    return __func()
 
-
-@scheduler_page.route('/<string:job_id>/<string:action>', methods=['POST'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/scheduler'])
-def scheduler_actions_by_id(job_id, action):
-    if action == "pause":
+def pause_job(job_id):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         running_context.controller.pause_job(job_id)
         current_app.logger.info('Scheduler paused job {0}'.format(job_id))
         return json.dumps({"status": "Job Paused"})
-    elif action == "resume":
+    return __func()
+
+def resume_job(job_id):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
         running_context.controller.resume_job(job_id)
         current_app.logger.info('Scheduler resumed job {0}'.format(job_id))
         return json.dumps({"status": "Job Resumed"})
-    current_app.logger.error('Scheduler cannot take action {0} on job {1}. No such action available'.format(action,
-                                                                                                            job_id))
-    return json.dumps({"status": "invalid command"})
+    return __func()
 
-
-@scheduler_page.route('/jobs', methods=['GET'])
-@auth_token_required
-# @roles_accepted(*userRoles["/execution/listener"])
-def get_scheduler():
-    return running_context.controller.get_scheduled_jobs()
+def get_jobs():
+    from server.context import running_context
+    #@roles_accepted(*running_context.user_roles['/execution/scheduler'])
+    def __func():
+        return running_context.controller.get_scheduled_jobs()
+    return __func()
--- server/endpoints/trigger.py
+++ server/endpoints/trigger.py
@@ -1,119 +1,118 @@
 import json
-from flask import Blueprint, request, current_app
-from flask_security import auth_token_required, roles_accepted
-from server.flaskserver import running_context
+from flask import request, current_app
+from flask_security import roles_accepted
 from server import forms
 
-triggers_page = Blueprint('triggers_page', __name__)
 
+def get_triggers():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        return json.dumps({"status": "success", "triggers": [trigger.as_json()
+                                                             for trigger in running_context.Triggers.query.all()]})
+    return __func()
 
-@triggers_page.route('/execute', methods=['POST'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener'])
 def listener():
-    form = forms.IncomingDataForm(request.form)
-    data_input = None
-    if form.validate():
-        if form.input.data:
-            data_input = json.loads(form.input.data)
-        returned_json = running_context.Triggers.execute(form.data.data, data_input)
-        current_app.logger.info('Executing triggers with conditional info {0} and input info {1}'.format(form.data.data,
-                                                                                                         data_input))
-        return json.dumps(returned_json)
-    else:
-        current_app.logger.error('Received invalid form for trigger execution')
-        return json.dumps({})
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        form = forms.IncomingDataForm(request.form)
+        data_input = None
+        if form.validate():
+            if form.input.data:
+                data_input = json.loads(form.input.data)
+            returned_json = running_context.Triggers.execute(form.data.data, data_input)
+            current_app.logger.info(
+                'Executing triggers with conditional info {0} and input info {1}'.format(form.data.data,
+                                                                                         data_input))
+            return json.dumps(returned_json)
+        else:
+            current_app.logger.error('Received invalid form for trigger execution')
+            return json.dumps({})
+    return __func()
 
+def create_trigger(trigger_name):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        form = forms.AddNewTriggerForm(request.form)
+        if form.validate():
+            query = running_context.Triggers.query.filter_by(name=trigger_name).first()
+            if query is None:
+                try:
+                    json.loads(form.conditional.data)
+                    running_context.db.session.add(
+                        running_context.Triggers(name=trigger_name,
+                                                 condition=form.conditional.data,
+                                                 playbook=form.playbook.data,
+                                                 workflow=form.workflow.data))
 
-@triggers_page.route('/triggers', methods=['GET'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener/triggers'])
-def display_all_triggers():
-    return json.dumps({"status": "success", "triggers": [trigger.as_json()
-                                                         for trigger in running_context.Triggers.query.all()]})
+                    running_context.db.session.commit()
+                    current_app.logger.info('Added trigger: '
+                                            '{0}'.format(json.dumps({"name": trigger_name,
+                                                                     "condition": form.conditional.data,
+                                                                     "workflow": "{0}-{1}".format(form.playbook.data,
+                                                                                                  form.workflow.data)})))
+                    return json.dumps({"status": "success"})
+                except ValueError:
+                    current_app.logger.error(
+                        'Cannot create trigger {0}. Invalid JSON in conditional field'.format(trigger_name))
+                    return json.dumps({"status": "error: invalid json in conditional field"})
+            else:
+                current_app.logger.warning('Cannot create trigger {0}. Trigger already exists'.format(trigger_name))
+                return json.dumps({"status": "warning: trigger with that name already exists"})
+        current_app.logger.error('Cannot create trigger {0}. Invalid form'.format(trigger_name))
+        return json.dumps({"status": "error: form not valid"})
+    return __func()
 
+def read_trigger(trigger_name):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        query = running_context.Triggers.query.filter_by(name=trigger_name).first()
+        if query:
+            return json.dumps({"status": 'success', "trigger": query.as_json()})
+        current_app.logger.error('Cannot display trigger {0}. Does not exist'.format(trigger_name))
+        return json.dumps({"status": "error: trigger not found"})
+    return __func()
 
-@triggers_page.route('/triggers/<string:name>', methods=['PUT'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener/triggers'])
-def create_trigger(name):
-    form = forms.AddNewTriggerForm(request.form)
-    if form.validate():
-        query = running_context.Triggers.query.filter_by(name=name).first()
-        if query is None:
-            try:
-                json.loads(form.conditional.data)
-                running_context.db.session.add(
-                    running_context.Triggers(name=name,
-                                             condition=form.conditional.data,
-                                             playbook=form.playbook.data,
-                                             workflow=form.workflow.data))
+def update_trigger(trigger_name):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        form = forms.EditTriggerForm(request.form)
+        trigger = running_context.Triggers.query.filter_by(name=trigger_name).first()
+        if form.validate() and trigger is not None:
+            # Ensures new name is unique
+            if form.name.data:
+                if len(running_context.Triggers.query.filter_by(name=form.name.data).all()) > 0:
+                    return json.dumps({"status": "error: duplicate names found. Trigger could not be edited"})
 
+            result = trigger.edit_trigger(form)
+
+            if result:
                 running_context.db.session.commit()
-                current_app.logger.info('Added trigger: '
-                                        '{0}'.format(json.dumps({"name": name,
-                                                                 "condition": form.conditional.data,
-                                                                 "workflow": "{0}-{1}".format(form.playbook.data,
-                                                                                              form.workflow.data)})))
+                current_app.logger.info('Edited trigger {0}'.format(trigger))
                 return json.dumps({"status": "success"})
-            except ValueError:
-                current_app.logger.error('Cannot create trigger {0}. Invalid JSON in conditional field'.format(name))
+            else:
+                current_app.logger.error('Could not edit trigger {0}. Malformed JSON in conditional'.format(trigger))
                 return json.dumps({"status": "error: invalid json in conditional field"})
-        else:
-            current_app.logger.warning('Cannot create trigger {0}. Trigger already exists'.format(name))
-            return json.dumps({"status": "warning: trigger with that name already exists"})
-    current_app.logger.error('Cannot create trigger {0}. Invalid form'.format(name))
-    return json.dumps({"status": "error: form not valid"})
+        current_app.logger.error('Could not edit trigger {0}. Form is invalid'.format(trigger))
+        return json.dumps({"status": "trigger could not be edited"})
+    return __func()
 
-
-@triggers_page.route('/triggers/<string:name>', methods=['GET'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener/triggers'])
-def read_trigger(name):
-    query = running_context.Triggers.query.filter_by(name=name).first()
-    if query:
-        return json.dumps({"status": 'success', "trigger": query.as_json()})
-    current_app.logger.error('Cannot display trigger {0}. Does not exist'.format(name))
-    return json.dumps({"status": "error: trigger not found"})
-
-
-@triggers_page.route('/triggers/<string:name>', methods=['POST'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener/triggers'])
-def update_trigger(name):
-    form = forms.EditTriggerForm(request.form)
-    trigger = running_context.Triggers.query.filter_by(name=name).first()
-    if form.validate() and trigger is not None:
-        # Ensures new name is unique
-        if form.name.data:
-            if len(running_context.Triggers.query.filter_by(name=form.name.data).all()) > 0:
-                return json.dumps({"status": "error: duplicate names found. Trigger could not be edited"})
-
-        result = trigger.edit_trigger(form)
-
-        if result:
+def delete_trigger(trigger_name):
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/triggers'])
+    def __func():
+        query = running_context.Triggers.query.filter_by(name=trigger_name).first()
+        if query:
+            running_context.Triggers.query.filter_by(name=trigger_name).delete()
             running_context.db.session.commit()
-            current_app.logger.info('Edited trigger {0}'.format(name))
+            current_app.logger.info('Deleted trigger {0}'.format(trigger_name))
             return json.dumps({"status": "success"})
-        else:
-            current_app.logger.error('Could not edit trigger {0}. Malformed JSON in conditional'.format(name))
-            return json.dumps({"status": "error: invalid json in conditional field"})
-    current_app.logger.error('Could not edit trigger {0}. Form is invalid'.format(name))
-    return json.dumps({"status": "trigger could not be edited"})
-
-
-@triggers_page.route('/triggers/<string:name>', methods=['DELETE'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/execution/listener/triggers'])
-def delete_trigger(name):
-    query = running_context.Triggers.query.filter_by(name=name).first()
-    if query:
-        running_context.Triggers.query.filter_by(name=name).delete()
-        running_context.db.session.commit()
-        current_app.logger.info('Deleted trigger {0}'.format(name))
-        return json.dumps({"status": "success"})
-    elif query is None:
-        current_app.logger.warning('Cannot delete trigger {0}. Trigger does not exist'.format(name))
-        return json.dumps({"status": "error: trigger does not exist"})
-    return json.dumps({"status": "error: could not remove trigger"})
-
+        elif query is None:
+            current_app.logger.warning('Cannot delete trigger {0}. Trigger does not exist'.format(trigger_name))
+            return json.dumps({"status": "error: trigger does not exist"})
+        return json.dumps({"status": "error: could not remove trigger"})
+    return __func()
