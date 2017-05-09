4b943953f68c02be8fab907d15025733239a6f62
Corey Robins <robins_corey@bah.com>
2017-05-09 13:20:48 -0400

    Metrics YAML

--- server/endpoints/metrics.py
+++ server/endpoints/metrics.py
@@ -1,12 +1,22 @@
-from flask import Blueprint
-from flask_security import auth_token_required, roles_accepted
+from flask_security import roles_accepted
 import server.metrics as metrics
-from server.flaskserver import running_context
 import json
 from copy import deepcopy
 
-metrics_page = Blueprint('metrics_page', __name__)
 
+def get_app_metrics():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/metrics'])
+    def __func():
+        return json.dumps(_convert_action_time_averages())
+    return __func()
+
+def get_workflow_metrics():
+    from server.context import running_context
+    @roles_accepted(*running_context.user_roles['/metrics'])
+    def __func():
+        return json.dumps(_convert_workflow_time_averages())
+    return __func()
 
 def _convert_action_time_averages():
     ret = deepcopy(metrics.app_metrics)
@@ -18,23 +28,8 @@ def _convert_action_time_averages():
                 action['error']['avg_time'] = str(action['error']['avg_time'])
     return ret
 
-
-@metrics_page.route('/apps', methods=['GET'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/metrics'])
-def get_app_metrics():
-    return json.dumps(_convert_action_time_averages())
-
-
 def _convert_workflow_time_averages():
     ret = deepcopy(metrics.workflow_metrics)
     for workflow in ret.values():
         workflow['avg_time'] = str(workflow['avg_time'])
     return ret
-
-
-@metrics_page.route('/workflows', methods=['GET'])
-@auth_token_required
-@roles_accepted(*running_context.user_roles['/metrics'])
-def get_workflow_metrics():
-    return json.dumps(_convert_workflow_time_averages())
