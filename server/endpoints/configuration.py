138f1060c8e0030e1c15f4009f1a50196cae285f
Corey Robins <robins_corey@bah.com>
2017-05-11 16:29:49 -0400

    Configuration consistency check

--- server/endpoints/configuration.py
+++ server/endpoints/configuration.py
@@ -5,7 +5,7 @@ import core.config.config
 import core.config.paths
 
 
-def config_values(key):
+def read_config_values(key):
     from server.context import running_context
     from server.flaskserver import current_user
 
@@ -26,7 +26,7 @@ def config_values(key):
     return __func()
 
 
-def set_configuration():
+def update_configuration():
     from server.context import running_context
     from server.flaskserver import current_user, write_playbook_to_file
 
@@ -34,29 +34,25 @@ def set_configuration():
     def __func():
         if current_user.is_authenticated:
             form = forms.SettingsForm(request.form)
-            if form.validate():
-                for key, value in form.data.items():
-                    if hasattr(core.config.paths, key):
-                        if key == 'workflows_path' and key != core.config.paths.workflows_path:
-                            for playbook in running_context.controller.get_all_playbooks():
-                                try:
-                                    write_playbook_to_file(playbook)
-                                except (IOError, OSError):
-                                    pass
-                            core.config.paths.workflows_path = value
-                            running_context.controller.workflows = {}
-                            running_context.controller.load_all_workflows_from_directory()
-                        else:
-                            setattr(core.config.paths, key, value)
-                            if key == 'apps_path':
-                                core.config.config.load_function_info()
+            for key, value in form.data.items():
+                if hasattr(core.config.paths, key):
+                    if key == 'workflows_path' and key != core.config.paths.workflows_path:
+                        for playbook in running_context.controller.get_all_playbooks():
+                            try:
+                                write_playbook_to_file(playbook)
+                            except (IOError, OSError):
+                                pass
+                        core.config.paths.workflows_path = value
+                        running_context.controller.workflows = {}
+                        running_context.controller.load_all_workflows_from_directory()
                     else:
-                        setattr(core.config.config, key, value)
-                current_app.logger.info('Changed configuration')
-                return {"status": 'success'}
-            else:
-                current_app.logger.error('Configuration change form is invalid: {0}'.format(form.__dict__))
-                return {"status": 'error: invalid form'}
+                        setattr(core.config.paths, key, value)
+                        if key == 'apps_path':
+                            core.config.config.load_function_info()
+                else:
+                    setattr(core.config.config, key, value)
+            current_app.logger.info('Changed configuration')
+            return {"status": 'success'}
         else:
             current_app.logger.warning('Configuration attempted to be set by non authenticated user')
             return {"status": 'error: user is not authenticated'}
