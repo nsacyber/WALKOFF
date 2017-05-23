25730eb9b4c07f357eebe358fe76f5c0112561fa
Corey Robins <robins_corey@bah.com>
2017-05-23 08:19:47 -0400

    Added ability to update triggers if a playbook and/or workflow name change occurs

--- server/triggers.py
+++ server/triggers.py
@@ -67,6 +67,22 @@ class Triggers(Base):
 
         return True
 
+    @staticmethod
+    def update_playbook_workflow(old_playbook, new_playbook='', old_workflow='', new_workflow=''):
+        if new_playbook and new_workflow:
+            triggers = Triggers.query.filter_by(playbook=old_playbook, workflow=old_workflow).all()
+            for trigger in triggers:
+                trigger.playbook = new_playbook
+                trigger.workflow = new_workflow
+        elif new_playbook:
+            triggers = Triggers.query.filter_by(playbook=old_playbook).all()
+            for trigger in triggers:
+                trigger.playbook = new_playbook
+        elif new_workflow:
+            triggers = Triggers.query.filter_by(workflow=old_workflow).all()
+            for trigger in triggers:
+                trigger.workflow = new_workflow
+
     def as_json(self):
         """ Gets the JSON representation of all the Trigger object.
         
