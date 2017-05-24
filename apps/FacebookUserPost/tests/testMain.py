237a4aff90541510027a60a933140f95209afcdb
Justin Tervala <Tervala_Justin@bah.com>
2017-05-24 15:30:13 -0400

    Anotehhr attempt to fix teh Python 3.6 facebok error

--- apps/FacebookUserPost/tests/testMain.py
+++ apps/FacebookUserPost/tests/testMain.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from apps.FacebookUserPost import main
+import sys
 
 
 class TestMain(TestCase):
@@ -11,5 +12,9 @@ class TestMain(TestCase):
                 "user_id": "me"}
         self.assertEqual(self.app.add_facebook_user(args), 0)
 
+        if sys.version_info[0] == 3 and sys.version_info[1] == 6
+            import gevent.monkey
+            gevent.monkey.patch_ssl()
+        
         args = {"message": "Explore WALKOFF\nat https://github.com/iadgov/WALKOFF\n :D :D :D :D :D"}
         self.assertTrue("id" in self.app.post_to_user_wall(args))
