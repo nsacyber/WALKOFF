8a5b3692bc13c65fae7f5bae8d1223ece94360a3
iadgovuser11 <iadgovuser11@users.noreply.github.com>
2017-05-24 07:59:10 -0400

    api points to custom schema

--- core/api.py
+++ core/api.py
@@ -1,18 +1,92 @@
-from connexion.apis.abstract import AbstractAPI
+#from connexion.apis.abstract import AbstractAPI
 import os.path
 from core.config.paths import apps_path
 from connexion.resolver import Resolver,Resolution
 from connexion.lifecycle import ConnexionRequest
 from connexion.apis.flask_api import Jsonifier
-from connexion.operation import Operation
-
-
+from connexion.apis import abstract
+from core.config import paths
 try:
     from urllib import urlencode
 except ImportError:
     from urllib.parse import urlencode
 
-class WalkoffAppDefinition(AbstractAPI, object):
+import functools
+from swagger_spec_validator.validator20 import validate_apis, validate_definitions, deref
+def validate_spec(spec_dict, spec_url='', http_handlers=None):
+    """Validates a Swagger 2.0 API Specification given a Swagger Spec.
+        :param spec_dict: the json dict of the swagger spec.
+        :type spec_dict: dict
+        :param spec_url: url from which spec_dict was retrieved. Used for
+            dereferencing refs. eg: file:///foo/swagger.json
+        :type spec_url: string
+        :param http_handlers: used to download any remote $refs in spec_dict with
+            a custom http client. Defaults to None in which case the default
+            http client built into jsonschema's RefResolver is used. This
+            is a mapping from uri scheme to a callable that takes a
+            uri.
+        :returns: the resolver (with cached remote refs) used during validation
+        :rtype: :class:`jsonschema.RefResolver`
+        :raises: :py:class:`swagger_spec_validator.SwaggerValidationError`
+        """
+    swagger_resolver = validate_json(
+        spec_dict,
+        'walkoff_schema.json',
+        spec_url=spec_url,
+        http_handlers=http_handlers,
+    )
+
+    bound_deref = functools.partial(deref, resolver=swagger_resolver)
+    spec_dict = bound_deref(spec_dict)
+    apis = bound_deref(spec_dict['paths'])
+    definitions = bound_deref(spec_dict.get('definitions', {}))
+    validate_apis(apis, bound_deref)
+    validate_definitions(definitions, bound_deref)
+    return swagger_resolver
+
+import json
+from swagger_spec_validator import ref_validators
+from jsonschema import RefResolver
+from jsonschema.validators import Draft4Validator
+def validate_json(spec_dict, schema_path, spec_url='', http_handlers=None):
+    """Validate a json document against a json schema.
+    :param spec_dict: json document in the form of a list or dict.
+    :param schema_path: package relative path of the json schema file.
+    :param spec_url: base uri to use when creating a
+        RefResolver for the passed in spec_dict.
+    :param http_handlers: used to download any remote $refs in spec_dict with
+        a custom http client. Defaults to None in which case the default
+        http client built into jsonschema's RefResolver is used. This
+        is a mapping from uri scheme to a callable that takes a
+        uri.
+    :return: RefResolver for spec_dict with cached remote $refs used during
+        validation.
+    :rtype: :class:`jsonschema.RefResolver`
+    """
+    schema_path = os.path.join(paths.schema_path, schema_path)
+    with open(schema_path) as schema_file:
+        schema = json.loads(schema_file.read())
+
+    schema_resolver = RefResolver('file://{0}'.format(schema_path), schema)
+
+    spec_resolver = RefResolver(spec_url, spec_dict,
+                                handlers=http_handlers or {})
+
+    ref_validators.validate(
+        spec_dict,
+        schema,
+        resolver=schema_resolver,
+        instance_cls=ref_validators.create_dereffing_validator(spec_resolver),
+        cls=Draft4Validator)
+
+    # Since remote $refs were downloaded, pass the resolver back to the caller
+    # so that its cached $refs can be re-used.
+    return spec_resolver
+
+
+abstract.validate_spec = validate_spec
+
+class WalkoffAppDefinition(abstract.AbstractAPI, object):
     def __init__(self, name, instance):
         self.name = name
         self.path = os.path.abspath(os.path.join(apps_path, name, "functions.yaml"))
