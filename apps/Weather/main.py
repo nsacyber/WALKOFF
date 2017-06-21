f24fccb2f664d78f7994172775ed7718bd92076e
Ryan Fischbach <Fischbrm@gmail.com>
2017-06-21 15:01:11 -0400

    added sunrise and sunset functionality to Weather App

--- apps/Weather/main.py
+++ apps/Weather/main.py
@@ -20,3 +20,43 @@ class Weather(App):
         print(weather)
         print(weather.get_temperature('fahrenheit')['temp'])
         return weather.get_temperature('fahrenheit')['temp']
+
+    @action
+    def get_sunrise(self, city):
+        observation = self.owm.weather_at_place('{0}, us'.format(city))
+        print(observation)
+        weather = observation.get_weather()
+        print(weather)
+        print(weather.get_sunrise_time(timeformat='unix'))
+        return weather.get_sunrise_time(timeformat='unix')
+
+    @action
+    def get_sunset(self, city):
+        observation = self.owm.weather_at_place('{0}, us'.format(city))
+        print(observation)
+        weather = observation.get_weather()
+        print(weather)
+        print(weather.get_sunset_time(timeformat='unix'))
+        return weather.get_sunset_time(timeformat='unix')
+
+    @action
+    def temp_color(temperature):
+    
+        result = temperature
+        if  (32 < result < 60):
+            print(result)
+            return "blue"
+
+        elif (60 < result < 70):
+            print(result)
+            return "yellow"
+
+        elif (70 < result < 80):
+            print(result)
+            return "orange"
+
+        elif (80 < result):
+            print(result)
+            return "red"
+        else:
+            return "green"
