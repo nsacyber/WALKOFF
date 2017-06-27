3803146262ec38cf5612a9a08eaa747993ba8477
shadowbq <shadowbq@gmail.com>
2017-06-26 23:51:49 -0400

    fix LC_COLLATE sort error on os.listdir

--- core/helpers.py
+++ core/helpers.py
@@ -18,11 +18,11 @@ logger = logging.getLogger(__name__)
 
 def import_py_file(module_name, path_to_file):
     """Dynamically imports a python module.
-    
+
     Args:
         module_name (str): The name of the module to be imported.
         path_to_file (str): The path to the module to be imported.
-        
+
     Returns:
         The module object that was imported.
     """
@@ -45,11 +45,11 @@ def import_py_file(module_name, path_to_file):
 
 def import_lib(directory, module_name):
     """Dynamically imports a Python library.
-    
+
     Args:
         directory (str): The directory in which the library is located.
         module_name (str): The name of the library to be imported.
-        
+
     Returns:
         The module object that was imported.
     """
@@ -67,10 +67,10 @@ def import_lib(directory, module_name):
 
 def construct_module_name_from_path(path):
     """Constructs the name of the module with the path name.
-    
+
     Args:
         path (str): The path to the module.
-        
+
     Returns:
          The name of the module with the path name.
     """
@@ -81,7 +81,7 @@ def construct_module_name_from_path(path):
 
 def import_app_main(app_name, path=None, reload=False):
     """Dynamically imports the main function of an App.
-    
+
     Args:
         app_name (str): The name of the App from which to import the main function.
         path (str, optional): The path to the apps module. Defaults to core.config.paths.apps_path
@@ -111,7 +111,9 @@ def import_app_main(app_name, path=None, reload=False):
 
 def __list_valid_directories(path):
     try:
-        return [f for f in os.listdir(path)
+        #NOTE: os.listdir is OS dependent, and sorts files accordingly. LC_COLLATE='C' will fail unless sorted.
+        sorted_filelist = sorted(os.listdir(path))
+        return [f for f in sorted_filelist
                 if (os.path.isdir(os.path.join(path, f))
                     and not f.startswith('__'))]
     except (IOError, OSError) as e:
@@ -121,10 +123,10 @@ def __list_valid_directories(path):
 
 def list_apps(path=None):
     """Get a list of the apps.
-    
+
     Args:
         path (str, optional): The path to the apps folder. Default is None.
-        
+
     Returns:
         A list of the apps given the apps path or the apps_path in the configuration.
     """
@@ -134,12 +136,12 @@ def list_apps(path=None):
 
 
 def list_widgets(app, app_path=None):
-    """Get a list of the widgets for a given app. 
-    
+    """Get a list of the widgets for a given app.
+
     Args:
         app (str): The app under which the widgets are located.
         app_path (str, optional): The path to the widgets folder. Default is None.
-        
+
     Returns:
         A list of the widgets given the apps path or the apps_path in the configuration.
     """
@@ -150,10 +152,10 @@ def list_widgets(app, app_path=None):
 
 def list_class_functions(class_name):
     """Get the functions for a python Class.
-    
+
     Args:
         class_name (str): The name of the python Class from which to get the functions.
-        
+
     Returns:
         The list of functions for a given python Class.
     """
@@ -163,10 +165,10 @@ def list_class_functions(class_name):
 
 def locate_workflows_in_directory(path=None):
     """Get a list of workflows in a specified directory or the workflows_path directory as specified in the configuration.
-    
+
     Args:
         path (str, optional): The directory path from which to locate the workflows. Defaults to None.
-        
+
     Returns:
         A list of workflow names from the specified path, or the directory specified in the configuration.
     """
@@ -181,10 +183,10 @@ def locate_workflows_in_directory(path=None):
 
 def get_workflow_names_from_file(filename):
     """Get a list of workflow names in a given file.
-    
+
     Args:
         filename (str): The filename from which to locate the workflows.
-        
+
     Returns:
         A list of workflow names from the specified file, if the file exists.
     """
@@ -198,11 +200,11 @@ __workflow_key_separator = '-'
 
 def construct_workflow_name_key(playbook, workflow):
     """Constructs a key for the workflow given the playbook name and workflow name.
-    
+
     Args:
         playbook (str): The playbook under which the workflow is located.
         workflow (str): The name of the workflow.
-        
+
     Returns:
         The key for the workflow given the playbook name and workflow name.
     """
@@ -211,11 +213,11 @@ def construct_workflow_name_key(playbook, workflow):
 
 def extract_workflow_name(workflow_key, playbook_name=''):
     """Extracts a workflow name from a given key.
-    
+
     Args:
         workflow_key (str): The constructed key of the workflow from the playbook name and workflow name.
         playbook_name (str, optional): The playbook under which the workflow is located.
-        
+
     Returns:
         The extracted workflow name.
     """
@@ -227,11 +229,11 @@ def extract_workflow_name(workflow_key, playbook_name=''):
 
 def combine_dicts(x, y):
     """Combines two dictionaries into one.
-    
+
     Args:
         x (dict): One dictionary to be merged.
         y (dict): The other dictionary to be merged with x.
-        
+
     Returns:
         The merged dictionary.
     """
@@ -242,12 +244,12 @@ def combine_dicts(x, y):
 
 def import_submodules(package, recursive=False):
     """Imports the submodules from a given package.
-    
+
     Args:
         package (str): The name of the package from which to import the submodules.
-        recursive (bool, optional): A boolean to determine whether or not to recursively load the submodules. 
+        recursive (bool, optional): A boolean to determine whether or not to recursively load the submodules.
             Defaults to False.
-                        
+
     Returns:
         A dictionary containing the imported module objects.
     """
@@ -477,4 +479,3 @@ def dereference_step_routing(input_, accumulator, message_prefix):
             return __get_step_from_reference(input_, accumulator, message_prefix)
         else:
             return input_
-
