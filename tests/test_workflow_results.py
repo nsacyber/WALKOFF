c82667ef99e63ac56f4221772c42e7de8de8e8c2
Justin Tervala <Tervala_Justin@bah.com>
2017-07-13 13:27:42 -0400

    Workflow results log to the case database

--- tests/test_workflow_results.py
+++ tests/test_workflow_results.py
@@ -1,28 +1,50 @@
 from core.helpers import construct_workflow_name_key
+import core.case.database as case_database
 from server import flaskserver
-import server.workflowresults
+from core.case.workflowresults import WorkflowResult
 from tests import config
 from tests.util.servertestcase import ServerTestCase
 
+
 class TestWorkflowResults(ServerTestCase):
     def setUp(self):
-        server.workflowresults.results.clear()
+        case_database.initialize()
 
-    def test_workflow_result_recording(self):
+    def tearDown(self):
+        case_database.case_db.tear_down()
+
+    def test_workflow_result_format(self):
         flaskserver.running_context.controller.load_workflows_from_file(path=config.test_workflows_path +
                                                                         'multiactionWorkflowTest.playbook')
-        multiaction_key = construct_workflow_name_key('multiactionWorkflowTest', 'multiactionWorkflow')
-        flaskserver.running_context.controller.execute_workflow('multiactionWorkflowTest', 'multiactionWorkflow')
+        construct_workflow_name_key('multiactionWorkflowTest', 'multiactionWorkflow')
+        uid = flaskserver.running_context.controller.execute_workflow('multiactionWorkflowTest', 'multiactionWorkflow')
         with flaskserver.running_context.flask_app.app_context():
             flaskserver.running_context.shutdown_threads()
-        self.assertEqual(len(server.workflowresults.results), 1)
-        key = list(server.workflowresults.results.keys())[0]
-        self.assertIn('status', server.workflowresults.results[key])
-        self.assertEqual(server.workflowresults.results[key]['status'], 'completed')
-        self.assertIn('name', server.workflowresults.results[key])
-        self.assertEqual(server.workflowresults.results[key]['name'], multiaction_key)
-        self.assertIn('completed_at', server.workflowresults.results[key])
-        self.assertIn('started_at', server.workflowresults.results[key])
-        self.assertIn('results', server.workflowresults.results[key])
-        self.assertEqual(len(server.workflowresults.results[key]['results']), 2)
-        self.assertEqual(len(server.workflowresults.results[key]['results']), 2)
+
+        workflow_results = case_database.case_db.session.query(WorkflowResult).all()
+        self.assertEqual(len(workflow_results), 1)
+        workflow_result = workflow_results[0]
+        self.assertEqual(workflow_result.uid, uid)
+        self.assertEqual(workflow_result.status, 'completed')
+        self.assertEqual(len(workflow_result.results.all()), 2)
+
+        def strip_timestamp(result):
+            result.pop('timestamp')
+            return result
+        self.assertDictEqual(strip_timestamp(workflow_result.results[0].as_json()),
+                             {'input': {},
+                              'type': 'success',
+                              'name': 'start',
+                              'result': {"status": "Success", "result": {"message": "HELLO WORLD"}}})
+
+    def test_workflow_result_multiple_workflows(self):
+        flaskserver.running_context.controller.load_workflows_from_file(path=config.test_workflows_path +
+                                                                             'multiactionWorkflowTest.playbook')
+        construct_workflow_name_key('multiactionWorkflowTest', 'multiactionWorkflow')
+        uid1 = flaskserver.running_context.controller.execute_workflow('multiactionWorkflowTest', 'multiactionWorkflow')
+        uid2 = flaskserver.running_context.controller.execute_workflow('multiactionWorkflowTest', 'multiactionWorkflow')
+        with flaskserver.running_context.flask_app.app_context():
+            flaskserver.running_context.shutdown_threads()
+
+        workflow_uids = case_database.case_db.session.query(WorkflowResult).with_entities(WorkflowResult.uid).all()
+        self.assertSetEqual({uid1, uid2}, {uid[0] for uid in workflow_uids})
