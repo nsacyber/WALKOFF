048a156cead15b1e7f74db8f6507e57ee6d221d0
iadgovuser11 <iadgovuser11@users.noreply.github.com>
2017-08-08 07:34:29 -0400

    Removed template and static files for all pages except login.html and index.html

--- server/static/pages/settings/main2.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var myApp = angular.module('settingsPageApp', [], function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{a');
-        $interpolateProvider.endSymbol('a}');
-    });
-});
