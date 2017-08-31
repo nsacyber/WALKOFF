ad911e2cf16c183c60308ffe3b5010b974fb7796
robins_corey@bah.com <robins_corey@bah.com>
2017-08-31 07:20:26 -0700

    Added generate_certificates script to make.py

--- make.py
+++ make.py
@@ -4,6 +4,10 @@ import os
 def main():
     print('\nInstalling Python Dependencies...')
     os.system('pip install -r requirements.txt')
+    os.system('python installDependencies.py')
+
+    print('\nGenerating Certificates...')
+    os.system('python generate_certificates.py')
 
     print('\nInstalling Node Packages...')
     os.chdir('./client')
