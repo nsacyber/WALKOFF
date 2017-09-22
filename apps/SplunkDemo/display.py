3cb37f1fff7619ecb67563a3490bc040b7e107dd
JustinTervala <Tervala_Justin@bah.com>
2017-09-22 12:19:07 -0700

    Added some fixes to some apps

--- apps/SplunkDemo/display.py
+++ apps/SplunkDemo/display.py
@@ -2,10 +2,9 @@ from flask import Blueprint
 from apps import AppBlueprint
 import os.path
 import json
-import os
 import core.case.database as case_database
 from core.case.workflowresults import WorkflowResult
-import pprint
+
 blueprint = AppBlueprint(blueprint=Blueprint('SplunkDemoPage', __name__))
 
 
@@ -17,10 +16,15 @@ def load(*args, **kwargs):
 
 default_data_file = os.path.join('.', 'apps', 'SplunkDemo', 'data', 'data.json')
 
+
 @blueprint.blueprint.route('/data', methods=['GET'])
 def get_data():
-   
-    workflow_result = case_database.case_db.session.query(WorkflowResult).all()[0]
+    if not os.path.exists(default_data_file):
+        return {'error': 'data not found'}, 404
+    with open(default_data_file, 'r') as file_in:
+        data_in = json.loads(file_in.read())
+
+    workflow_result = case_database.case_db.session.query(WorkflowResult).filter(WorkflowResult.uid == 'INSERT UID HERE').first()
     unrolled_steps = []
     for step_result in workflow_result.results:
         if step_result.app != 'Walkoff' or step_result.action != 'wait for workflow completion':
@@ -28,25 +32,17 @@ def get_data():
             step_as_json['location'] = 'local'
             unrolled_steps.append(step_as_json)
         else:
-            remote_workflowresults = step_result.result # this will itself be a workflowResults object
-            remote_workflowresults = json.loads(remote_workflowresults)['result']
-            for step in remote_workflowresults['results']:
+            remote_workflowresults = step_result.results # this will itself be a workflowResults object
+            remote_steps_results = remote_workflowresults['results']
+            for step in remote_steps_results:
                 step['location'] = 'remote'
                 unrolled_steps.append(step)
-    workflow_results = unrolled_steps
-    echo_step = next((step for step in unrolled_steps if step['action'] == 'echo object'), None)
-    data = echo_step['input']['data']
-    print(data)
-    response = {'workflow_results': unrolled_steps, 
-                'pcap_name': 'capture.pcap',
-                'exe_name': 'quanrantined_malware.bin'}
-    response.update(data)
-    # update with pcap and bin
-    return response
 
-@blueprint.blueprint.route('/view_pcap', methods=['GET'])
-def view_in_wireshark():
-    os.system('wireshark -r /home/ubunutu/WALKOFF/SplunkDemo/data/capture.pcap &')
+    data_in.update({'workflow_results': unrolled_steps})
+    # update with pcap and bin
+    return data_in
+
+
 
 
 
