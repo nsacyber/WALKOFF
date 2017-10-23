9f48227b1f473716ceb78dbfaa9963246b695358
Justin Tervala <Tervala_Justin@bah.com>
2017-10-23 16:02:28 -0400

    JsonElementCreator works with triggers; its tests are broken

--- core/jsonelementcreator.py
+++ core/jsonelementcreator.py
@@ -1,3 +1,10 @@
+from collections import OrderedDict
+from future.utils import iteritems
+import logging
+
+logging.basicConfig()
+
+
 class JsonElementCreator(object):
     playbook_class_ordering = None
 
@@ -7,20 +14,21 @@ class JsonElementCreator(object):
         cls._setup_ordering()
         if element_class is None:
             element_class = Playbook
-        class_iterator = iter(cls.playbook_class_ordering)
-        current_class, current_subfield_name = next(class_iterator)
+        class_iterator = iteritems(cls.playbook_class_ordering)
+        current_class, subfield_lookup = next(class_iterator)
         while current_class != element_class:
             try:
-                current_class, current_subfield_name = next(class_iterator)
+                current_class, subfield_lookup = next(class_iterator)
             except StopIteration:
                 raise ValueError('Unknown class {}'.format(element_class.__class__.__name__))
         try:
-            if current_subfield_name is not None:
-                next_class, _ = next(class_iterator)
-                subfield_json = json_in[current_subfield_name]
-                json_in[current_subfield_name] = [next_class.create(element_json) for element_json in subfield_json]
-                if hasattr(current_class, '_templatable'):
-                    json_in['raw_representation'] = dict(json_in)
+            if subfield_lookup is not None:
+                for subfield_name, next_class in subfield_lookup.items():
+                    if subfield_name in json_in:
+                        subfield_json = json_in[subfield_name]
+                        if hasattr(current_class, '_templatable'):
+                            json_in['raw_representation'] = dict(json_in)
+                        json_in[subfield_name] = [next_class.create(element_json) for element_json in subfield_json]
             return current_class(**json_in)
         except (KeyError, TypeError) as e:
             from core.helpers import format_exception_message
@@ -35,6 +43,11 @@ class JsonElementCreator(object):
             from core.executionelements.nextstep import NextStep
             from core.executionelements.flag import Flag
             from core.executionelements.filter import Filter
-            cls.playbook_class_ordering = (
-                (Playbook, 'workflows'), (Workflow, 'steps'), (Step, 'next_steps'), (NextStep, 'flags'),
-                (Flag, 'filters'), (Filter, None))
+            cls.playbook_class_ordering = OrderedDict([
+                (Playbook, {'workflows': Workflow}),
+                (Workflow, {'steps': Step}),
+                (Step, {'next_steps': NextStep, 'triggers': Flag}),
+                (NextStep, {'flags': Flag}),
+                (Flag, {'filters': Filter}),
+                (Filter, None)
+            ])
