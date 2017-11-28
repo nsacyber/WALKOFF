b742f1f7fd5994357dee0eb6c86a73d82936c49c
Justin Tervala <Tervala_Justin@bah.com>
2017-11-28 16:18:26 -0500

    Refactored walkoff.py; made a small banner on start

--- walkoff.py
+++ walkoff.py
@@ -17,9 +17,10 @@ def setup_logger():
         try:
             with open(paths.logging_config_path, 'rt') as log_config_file:
                 log_config = json.loads(log_config_file.read())
-        except:
+        except (IOError, OSError):
+            print('Could not read logging JSON file {}'.format(paths.logging_config_path))
+        except ValueError:
             print('Invalid JSON in logging config file')
-            pass
     else:
         print('No logging config found')
 
@@ -33,6 +34,7 @@ def setup_logger():
 def run():
     from core.multiprocessedexecutor.multiprocessedexecutor import spawn_worker_processes
     setup_logger()
+    print_banner()
     pids = spawn_worker_processes()
     monkey.patch_all()
     from server import flaskserver
@@ -47,21 +49,34 @@ def run():
     try:
         port = int(config.port)
     except ValueError:
-        print('Invalid port {0}. Port must be an integer'.format(config.port))
+        logger.fatal('Invalid port {}. Port must be an integer'.format(config.port))
     else:
-        host = config.host
-        if isfile(paths.certificate_path) and isfile(paths.private_key_path):
-            server = pywsgi.WSGIServer((host, port), application=flaskserver.app,
-                                       keyfile=paths.private_key_path, certfile=paths.certificate_path)
-            proto = 'https'
-        else:
-            server = pywsgi.WSGIServer((host, port), application=flaskserver.app)
-            proto = 'http'
-        from core.config.config import walkoff_version
-        logger.info('*** Running WALKOFF v.{} ***'.format(walkoff_version))
-        logger.info('Listening on host {0}://{1}:{2}'.format(proto, host, port))
-
+        server = setup_server(flaskserver.app, port)
         server.serve_forever()
+
+
+def print_banner():
+    from core.config.config import walkoff_version
+    banner = '***** Running WALKOFF v.{} *****'.format(walkoff_version)
+    header_footer_banner = '*'*len(banner)
+    logger.info(header_footer_banner)
+    logger.info(banner)
+    logger.info(header_footer_banner)
+
+
+def setup_server(app, port):
+    host = config.host
+    if isfile(paths.certificate_path) and isfile(paths.private_key_path):
+        server = pywsgi.WSGIServer((host, port), application=app,
+                                   keyfile=paths.private_key_path, certfile=paths.certificate_path)
+        protocol = 'https'
+    else:
+        logger.warning('Cannot find certificates. Using HTTP')
+        server = pywsgi.WSGIServer((host, port), application=app)
+        protocol = 'http'
+
+    logger.info('Listening on host {0}://{1}:{2}'.format(protocol, host, port))
+    return server
 
 
 if __name__ == "__main__":
