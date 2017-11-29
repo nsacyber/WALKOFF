1ac8374307e99b424ea6ec64ecd182f709e3c3b3
Jeremy Sestok <jsestok@bahvm16142717.resource.ds.bah.com>
2017-11-29 12:59:44 -0500

    setting up sample SSE for hello world workflow tutorial

--- interfaces/HelloWorld/interface/static/main.js
+++ interfaces/HelloWorld/interface/static/main.js
@@ -7,18 +7,37 @@ $(function () {
        'url': "interfaces/HelloWorld/metrics",
        'success': function(data) {
             for (const action in data) {
-                console.log(action)
-                console.log(data[action])
-                $('#results').append(`
+                $('#results tbody').append(`
                     <tr>
                         <td>${action}</td>
                         <td>${data[action]}</td>
                     </tr>
-                `)
+                `);
             }
        },
        'error': function(e) {
-            console.log(e)
+            console.error(e);
        }
-    })
+    });
+
+    const eventSource = new EventSource('interfaces/HelloWorld/actionstream?access_token=' + sessionStorage.getItem('access_token'));
+
+    function eventHandler(message) {
+        let result = JSON.parse(message.data);
+        
+        $('#actionResults tbody').append(`
+            <tr>
+                <td>${result.sender_uid}</td>
+                <td>${result.sender_name}</td>
+                <td>${result.timestamp}</td>
+                <td>${result.data.status}</td>
+            </tr>
+        `);
+    }
+
+    eventSource.addEventListener('action_success', eventHandler);
+    eventSource.addEventListener('action_error', eventHandler);
+    eventSource.addEventListener('error', (err) => {
+        console.error(err);
+    });
 })
