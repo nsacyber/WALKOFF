cc1ad07fd4a9d8e9e591dbd7f46cc120ec34c3a4
adpham95 <32873177+adpham95@users.noreply.github.com>
2017-12-29 10:09:05 -0500

    update.py can now take arguments instead of only being interactive

--- update.py
+++ update.py
@@ -1,9 +1,11 @@
 import os
+import ctypes as cts
 import sys
 from distutils.util import strtobool
 import subprocess
 import shutil
 import time
+import argparse
 import setup_walkoff
 import scripts.migrate_api
 import scripts.migrate_workflows
@@ -11,6 +13,7 @@ from walkoff import __version__ as version
 
 
 def prompt(question):
+
     while True:
         sys.stdout.write("\n* " + question + " yes/no? ")
         try:
@@ -19,7 +22,13 @@ def prompt(question):
             print("Please respond with 'yes' or 'no'.")
 
 
-def archive():
+def archive(flagged, inter):
+
+    if inter and not prompt("Do you want to make a backup of the current directory?"):
+        return
+    elif not flagged:
+        return
+
     if not os.path.exists("backups"):
         os.makedirs("backups")
 
@@ -36,13 +45,25 @@ def archive():
     print("Backup created at " + filename)
 
 
-def git():
+def git(flagged, inter):
+
+    if inter and not prompt("Do you want to git pull from the current branch?"):
+        return
+    elif not flagged:
+        return
+
     print("Pulling from current branch: ")
     print(subprocess.check_output(["git", "branch"], stderr=subprocess.STDOUT))
     print(subprocess.check_output(["git", "pull"], stderr=subprocess.STDOUT))
 
 
-def clean_pycache():
+def clean_pycache(flagged, inter):
+
+    if inter and not prompt("Do you want to clear pycache files?"):
+        return
+    elif not flagged:
+        return
+
     my_dir = os.path.dirname(os.path.abspath(__file__))
 
     for root, dirnames, filenames in os.walk(my_dir):
@@ -52,7 +73,43 @@ def clean_pycache():
                 os.remove(os.path.join(root, filename))
 
 
-def alembic():
+def setup(flagged, inter):
+
+    if inter and not prompt("Do you want to setup WALKOFF now?"):
+        return
+    elif not flagged:
+        return
+
+    setup_walkoff.main()
+
+
+def migrate_apps(flagged, inter):
+
+    if inter and not prompt("Do you want to migrate your app APIs?"):
+        return
+    elif not flagged:
+        return
+
+    scripts.migrate_api.main()
+
+
+def migrate_workflows(flagged, inter):
+
+    if inter and not prompt("Do you want to migrate your workflows?"):
+        return
+    elif not flagged:
+        return
+
+    scripts.migrate_workflows.main()
+
+
+def alembic(flagged, inter):
+
+    if inter and not prompt("Do you want alembic to migrate databases? (This will install alembic.)"):
+        return
+    elif not flagged:
+        return
+
     for i in range(0, 1):
         try:
             r = (subprocess.check_output(["alembic", "current"], stderr=subprocess.STDOUT))
@@ -71,20 +128,63 @@ def alembic():
 
 
 def main():
-    if prompt("Do you want to make a backup of the current directory?"):
-        archive()
-    if prompt("Do you want to git pull from the current branch?"):
-        git()
-    if prompt("Do you want to clear pycache files?"):
-        clean_pycache()
-    if prompt("Do you want to setup WALKOFF now?"):
-        setup_walkoff.main()
-    if prompt("Do you want to migrate your app apis?"):
-        scripts.migrate_api.main()
-    if prompt("Do you want to migrate your workflows?"):
-        scripts.migrate_workflows.main()
-    if prompt("Do you wish to use alembic to migrate databases? (This will install alembic if you don't have it.)"):
-        alembic()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interactive",
+                        help="Interactively prompt 'yes or no' for each choice.",
+                        action="store_true")
+
+    parser.add_argument("-e", "--everything",
+                        help="Equivalent to using all flags",
+                        action="store_true")
+
+    parser.add_argument("-a", "--archive",
+                        help="Creates a backup archive of the entire WALKOFF directory in backups/",
+                        action="store_true")
+
+    parser.add_argument("-p", "--pull",
+                        help="Performs a 'git pull' from the currently set branch",
+                        action="store_true")
+
+    parser.add_argument("-c", "--clean",
+                        help="Removes all .pyc and .pyo cache files.",
+                        action="store_true")
+
+    parser.add_argument("-s", "--setup",
+                        help="Performs WALKOFF setup. Requires root/administrator privileges.",
+                        action="store_true")
+
+    parser.add_argument("-ma", "--migrateapps",
+                        help="Runs app API migration script. Not reversible at this time.",
+                        action="store_true")
+
+    parser.add_argument("-mw", "--migrateworkflows",
+                        help="Runs workflow migration script. Not reversible at this time.",
+                        action="store_true")
+
+    parser.add_argument("-md", "--migratedatabase",
+                        help="Runs alembic database migration.",
+                        action="store_true")
+
+    if not len(sys.argv) > 1:
+        parser.print_help()
+        print("\nPlease specify at least one of the above arguments.")
+        return
+
+    args = parser.parse_args()
+
+    if args.everything or args.setup or args.migratedatabase:
+        if (os.name == 'posix' and os.geteuid() != 0) or (os.name == 'nt' and cts.windll.shell32.IsUserAnAdmin() != 0):
+            if not prompt("Using --setup or --migratedatabase requires root/administrator. Try anyways?"):
+                return
+
+    archive(args.everything or args.archive, args.interactive)
+    git(args.everything or args.pull, args.interactive)
+    clean_pycache(args.everything or args.clean, args.interactive)
+    setup(args.everything or args.setup, args.interactive)
+    migrate_apps(args.everything or args.migrateapps, args.interactive)
+    migrate_workflows(args.everything or args.migrateworkflows, args.interactive)
+    alembic(args.everything or args.migratedatabase, args.interactive)
 
 
 if __name__ == '__main__':
