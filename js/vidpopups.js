f12b28784c9019511ce283ef531e85ca05ce5610
iadgovuser11 <iadgovuser11@users.noreply.github.com>
2018-01-25 07:27:54 -0500

    New Splash Page

---
+++ js/vidpopups.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#video-modal').on('hidden.bs.modal', function () {
+        document.getElementById('player').pause();
+        document.getElementById('player').currentTime = 0;
+    });
+});
+
+function setSrc(path) {
+    document.getElementById("player").src = path
+}
+
