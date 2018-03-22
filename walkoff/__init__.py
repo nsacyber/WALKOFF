b8540e839f8141d0c2d7a5149b7186d17eaa5daa
Justin Tervala <Tervala_Justin@bah.com>
2018-03-22 12:31:26 -0400

    Preparing for version 0.8.0

--- walkoff/__init__.py
+++ walkoff/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.7.4'
+__version__ = '0.8.0'
 
 
 def initialize_databases():
--- walkoff/helpers.py
+++ walkoff/helpers.py
@@ -402,7 +402,7 @@ def convert_action_argument(argument):
 
 def create_sse_event(event_id=None, event=None, data=None):
     warnings.warn('create_sse_event is deprecated. Please use the walkoff.sse.SseStream class to construct SSE streams.'
-                  ' This function will be removed in version 0.9.0',
+                  ' This function will be removed in version 0.10.0',
                   DeprecationWarning)
     if data is None and event_id is None and event is None:
         return ''
