61195e3126ab7782dfa3d8bdeb16ab8b4f6b3df9
corey <robins_corey@bah.com>
2018-11-21 16:59:19 -0500

    Fixed runtimes to work with new database table

--- runtimes/python/syncruntime.py
+++ runtimes/python/syncruntime.py
@@ -9,8 +9,6 @@ import ujson
 import yaml
 from jsonschema import validate, ValidationError
 
-import sys
-sys.path.append(os.path.abspath('.'))
 import walkoff.config
 from apps import App
 from walkoff.appgateway import cache_apps, get_app
