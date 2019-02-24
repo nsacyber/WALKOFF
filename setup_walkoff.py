8a3eea218589358ce6b02f23e90f1348b0abbec6
Brian <brian.hart@xylogix.net>
2019-02-24 14:49:22 -0500

    Update setup_walkoff.py to use python3
    Per the new requirement to use Python 3 only

--- setup_walkoff.py
+++ setup_walkoff.py
@@ -4,10 +4,10 @@ import os
 def main():
     print('\nInstalling Python Dependencies...')
     os.system('pip install -r requirements.txt')
-    os.system('python scripts/install_dependencies.py')
+    os.system('python3 scripts/install_dependencies.py')
 
     print('\nGenerating Certificates...')
-    os.system('python scripts/generate_certificates.py')
+    os.system('python3 scripts/generate_certificates.py')
 
 if __name__ == '__main__':
     main()
