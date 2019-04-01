6717e66f9eaed64f7209f1d9295e98cc177d89b5
adpham95 <32873177+adpham95@users.noreply.github.com>
2019-04-01 14:33:09 -0400

    removed nested funcs from users endpoints

--- api_gateway/server/endpoints/users.py
+++ api_gateway/server/endpoints/users.py
@@ -12,80 +12,62 @@ from api_gateway.serverdb.user import User
 with_user = with_resource_factory('user', lambda user_id: User.query.filter_by(id=user_id).first())
 
 
+@jwt_required
+@permissions_accepted_for_resources(ResourcePermissions('users', ['read']))
 def read_all_users():
-    @jwt_required
-    @permissions_accepted_for_resources(ResourcePermissions('users', ['read']))
-    def __func():
-        page = request.args.get('page', 1, type=int)
-        return [user.as_json() for user in
-                User.query.paginate(page, current_app.config['ITEMS_PER_PAGE'], False).items], HTTPStatus.OK
-
-    return __func()
+    page = request.args.get('page', 1, type=int)
+    return [user.as_json() for user in
+            User.query.paginate(page, current_app.config['ITEMS_PER_PAGE'], False).items], HTTPStatus.OK
 
 
+@jwt_required
+@permissions_accepted_for_resources(ResourcePermissions('users', ['create']))
 def create_user():
-    @jwt_required
-    @permissions_accepted_for_resources(ResourcePermissions('users', ['create']))
-    def __func():
-        data = request.get_json()
-        username = data['username']
-        if not User.query.filter_by(username=username).first():
-            user = add_user(username=username, password=data['password'])
+    data = request.get_json()
+    username = data['username']
+    if not User.query.filter_by(username=username).first():
+        user = add_user(username=username, password=data['password'])
 
-            if 'roles' in data or 'active' in data:
-                role_update_user_fields(data, user)
+        if 'roles' in data or 'active' in data:
+            role_update_user_fields(data, user)
 
-            db.session.commit()
-            current_app.logger.info(f'User added: {user.as_json()}')
-            return user.as_json(), HTTPStatus.CREATED
-        else:
-            current_app.logger.warning(f'Cannot create user {username}. User already exists.')
-            return Problem.from_crud_resource(
-                HTTPStatus.BAD_REQUEST,
-                'user',
-                'create',
-                f'User with username {username} already exists')
-
-    return __func()
+        db.session.commit()
+        current_app.logger.info(f'User added: {user.as_json()}')
+        return user.as_json(), HTTPStatus.CREATED
+    else:
+        current_app.logger.warning(f'Cannot create user {username}. User already exists.')
+        return Problem.from_crud_resource(
+            HTTPStatus.BAD_REQUEST,
+            'user',
+            'create',
+            f'User with username {username} already exists')
 
 
+@jwt_required
+@permissions_accepted_for_resources(ResourcePermissions('users', ['read']))
+@with_user('read', 'user_id')
 def read_user(user_id):
-    @jwt_required
-    @permissions_accepted_for_resources(ResourcePermissions('users', ['read']))
-    @with_user('read', user_id)
-    def __func(user):
-        return user.as_json(), HTTPStatus.OK
-
-    return __func()
+    return user_id.as_json(), HTTPStatus.OK
 
 
-def update_user():
-    user_id = request.get_json()['id']
-
-    @jwt_required
-    @with_user('update', user_id)
-    def __func(user):
-        data = request.get_json()
-        current_user = get_jwt_identity()
-        if user.id == current_user:
-            return update_user_fields(data, user)
+@jwt_required
+@with_user('update', 'user_id')
+def update_user(user_id):
+    data = request.get_json()
+    current_user = get_jwt_identity()
+    if user_id.id == current_user:
+        return update_user_fields(data, user_id)
+    else:
+        response = role_update_user_fields(data, user_id, update=True)
+        if isinstance(response, tuple) and response[1] == HTTPStatus.FORBIDDEN:
+            current_app.logger.error(f"User {current_user} does not have permission to update user {user_id.id}")
+            return Problem.from_crud_resource(
+                HTTPStatus.FORBIDDEN,
+                'user',
+                'update',
+                f"Current user does not have permission to update user {user_id.id}.")
         else:
-            response = role_update_user_fields(data, user, update=True)
-            if isinstance(response, tuple) and response[1] == HTTPStatus.FORBIDDEN:
-                current_app.logger.error(f"User {current_user} does not have permission to update user {user.id}")
-                return Problem.from_crud_resource(
-                    HTTPStatus.FORBIDDEN,
-                    'user',
-                    'update',
-                    f"Current user does not have permission to update user {user_id}.")
-            else:
-                return response
-
-    return __func()
-
-
-def patch_user():
-    return update_user()
+            return response
 
 
 @admin_required
@@ -122,19 +104,17 @@ def update_user_fields(data, user):
     return user.as_json(), HTTPStatus.OK
 
 
+@jwt_required
+@permissions_accepted_for_resources(ResourcePermissions('users', ['delete']))
+@with_user('delete', 'user_id')
 def delete_user(user_id):
-    @jwt_required
-    @permissions_accepted_for_resources(ResourcePermissions('users', ['delete']))
-    @with_user('delete', user_id)
-    def __func(user):
-        if user.id != get_jwt_identity():
-            db.session.delete(user)
-            db.session.commit()
-            current_app.logger.info(f"User {user.username} deleted")
-            return None, HTTPStatus.NO_CONTENT
-        else:
-            current_app.logger.error(f"Could not delete user {user.id}. User is current user.")
-            return Problem.from_crud_resource(HTTPStatus.FORBIDDEN, 'user', 'delete',
-                                              'Current user cannot delete self.')
+    if user_id.id != get_jwt_identity():
+        db.session.delete(user_id)
+        db.session.commit()
+        current_app.logger.info(f"User {user_id.username} deleted")
+        return None, HTTPStatus.NO_CONTENT
+    else:
+        current_app.logger.error(f"Could not delete user {user_id.id}. User is current user.")
+        return Problem.from_crud_resource(HTTPStatus.FORBIDDEN, 'user', 'delete',
+                                          'Current user cannot delete self.')
 
-    return __func()
