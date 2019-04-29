df2444de22182c56aa2e9046e7f398b0d7506509
Hannah Burke <burke_hannah@bah.com>
2019-04-29 10:16:40 -0400

    added ssh

--- apps/ssh/1.0.0/src/app.py
+++ apps/ssh/1.0.0/src/app.py
@@ -19,53 +19,52 @@ class SSH(AppBase):
         super().__init__(redis, logger, console_logger)
 
 
-    # async def exec_command(self, args, hosts, port, username, password):
-    #     results = {}
+    async def exec_command(self, args, hosts, port, username, password):
+        results = {}
 
-    #     try:
-    #         for host in hosts:
-    #             try:
-    #                 async with asyncssh.connect(host=host,  port=port, username=username, password=password, known_hosts=None) as conn:
-    #                     for cmd in args:
-    #                         try:
-    #                             temp = await conn.run(cmd)
-    #                             output = temp.stdout
-    #                             results[host] = {"stdout": output, "stderr": ""}
-    #             except Exception as e:
-    #                 results[host] = {"stdout": "", "stderr": f"{e}"}
+        try:
+            for host in hosts:
+                try:
+                    async with asyncssh.connect(host=host,  port=port, username=username, password=password, known_hosts=None) as conn:
+                        for cmd in args:
+                            temp = await conn.run(cmd)
+                            output = temp.stdout
+                            results[host] = {"stdout": output, "stderr": ""}
+                except Exception as e:
+                    results[host] = {"stdout": "", "stderr": f"{e}"}
             
-    #         return results, 'Success'
+            return results, 'Success'
 
-    #     except Exception as e:
-    #         return 'ERROR', e
+        except Exception as e:
+            return 'ERROR', e
 
 
-    # async def sftp_copy(self, src_path, dest_path, src_host, src_port, src_username, src_password, dest_host, dest_port, dest_username, dest_password):
-    #     curr_dir = os.getcwd()
-    #     temp_dir = os.path.join(curr_dir, r'temp_data')
-    #     os.makedirs(temp_dir)
+    async def sftp_copy(self, src_path, dest_path, src_host, src_port, src_username, src_password, dest_host, dest_port, dest_username, dest_password):
+        curr_dir = os.getcwd()
+        temp_dir = os.path.join(curr_dir, r'temp_data')
+        os.makedirs(temp_dir)
 
-    #     async with asyncssh.connect(host=src_host,  port=src_port, username=src_username, password=src_password, known_hosts=None) as conn:
-    #         async with asyncssh.connect(host=dest_host,  port=dest_port, username=dest_username, password=dest_password, tunnel=conn, known_hosts=None) as tunneled_conn:
-    #             # grab remote file, place in container
-    #             async with conn.start_sftp_client() as sftp:
-    #                 results = await sftp.get(src_path, temp_dir)
+        async with asyncssh.connect(host=src_host,  port=src_port, username=src_username, password=src_password, known_hosts=None) as conn:
+            async with asyncssh.connect(host=dest_host,  port=dest_port, username=dest_username, password=dest_password, tunnel=conn, known_hosts=None) as tunneled_conn:
+                # grab remote file, place in container
+                async with conn.start_sftp_client() as sftp:
+                    results = await sftp.get(src_path, temp_dir)
 
-    #             spliced_path = src_path.split('/')
-    #             file_name = spliced_path[len(spliced_path) - 1]
+                spliced_path = src_path.split('/')
+                file_name = spliced_path[len(spliced_path) - 1]
 
-    #             # copy grabbed file to desired location
-    #             async with tunneled_conn.start_sftp_client() as sftp2:
-    #                 results2 = await sftp2.put(temp_dir + "/" + file_name, dest_path)
+                # copy grabbed file to desired location
+                async with tunneled_conn.start_sftp_client() as sftp2:
+                    results2 = await sftp2.put(temp_dir + "/" + file_name, dest_path)
 
-    #     # clearning up temp file
-    #     for file in os.listdir(temp_dir):
-    #         file_path = os.path.join(temp_dir, file)
-    #         if os.path.isfile(file_path):
-    #             os.remove(file_path)
-    #     os.rmdir(temp_dir)
+        # clearning up temp file
+        for file in os.listdir(temp_dir):
+            file_path = os.path.join(temp_dir, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        os.rmdir(temp_dir)
 
-    #     return "Success"
+        return "Success"
 
 
     async def run_shell_script_file(self, local_file_name, hosts, port, username, password):
