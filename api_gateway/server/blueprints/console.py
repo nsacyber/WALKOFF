e1bae4a941ca98c4069d508c0cb6da98a74b52e2
adpham95 <32873177+adpham95@users.noreply.github.com>
2019-05-09 10:25:31 -0400

    fixed console log endpoint being restricted to post

--- api_gateway/server/blueprints/console.py
+++ api_gateway/server/blueprints/console.py
@@ -16,7 +16,7 @@ console_stream_subs = {}
 console_stream_lock = RLock()
 
 
-@console_page.route('/log', methods=['POST'])
+@console_page.route('/log')
 # @jwt_required
 # @permissions_accepted_for_resources(ResourcePermissions("consolelog", ["create"]))
 def create_console_message():
