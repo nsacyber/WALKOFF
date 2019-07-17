b6bf6cbec2df6e07ae04fad0178167218e46316e
Hannah Burke <burke_hannah@bah.com>
2019-07-17 13:19:49 -0400

    got delete and update for globals wroking

--- api_gateway/executiondb/global_variable.py
+++ api_gateway/executiondb/global_variable.py
@@ -54,11 +54,8 @@ class GlobalVariable(Base):
     # Columns specific to GlobalVariables
     description = Column(String(255), default="")
     schema_id = Column(UUID(as_uuid=True), ForeignKey('global_variable_template.id_', ondelete='CASCADE'))
-<<<<<<< HEAD
-    #update_permission = Column(ARRAY(String(255)))
-=======
+    #update_permissions = Column(ARRAY(String(255)))
     _walkoff_type = Column(String(80), default="variable")
->>>>>>> aeb75858d0110f8e9797bc553f4819c23d3c9449
 
 
 class GlobalVariableTemplateSchema(BaseSchema):
