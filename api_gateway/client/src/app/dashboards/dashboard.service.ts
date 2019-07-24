e2f9d46b1277af1277c6576215e857b2f2bab359
tschmitthab <602646@bah.com>
2019-07-24 11:05:30 -0400

    Main and Dashboard serice.ts file changed

--- api_gateway/client/src/app/dashboards/dashboard.service.ts
+++ api_gateway/client/src/app/dashboards/dashboard.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { plainToClass, classToPlain, serialize, deserializeArray } from 'class-transformer';
 import { UtilitiesService } from '../utilities.service';
 import { HttpClient } from '@angular/common/http';
-import { Dashboard } from '../models/dashboard/dashboard';
+import { Report } from '../models/report/report';
 import { ExecutionService } from '../execution/execution.service';
-import { DashboardWidget } from '../models/dashboard/dashboardWidget';
+import { ReportWidget } from '../models/report/reportWidget';
 import { WorkflowStatus } from '../models/execution/workflowStatus';
 
 import * as csv from 'csvtojson';
@@ -13,67 +13,67 @@ import { Observable, Subscriber } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
-export class DashboardService {
+export class ReportService {
 
-    dashboardsChange: Observable<any>;
+    reportsChange: Observable<any>;
     observer: Subscriber<any>;
 
     constructor(private http: HttpClient, private utils: UtilitiesService, private executionService: ExecutionService) {
-        this.dashboardsChange = new Observable((observer) => {
+        this.reportsChange = new Observable((observer) => {
             this.observer = observer;
-            this.getDashboards().then(dashboards => this.observer.next(dashboards));
+            this.getReports().then(reports => this.observer.next(reports));
         })
     }
 
     emitChange(data: any) {
-        if (this.observer) this.getDashboards().then(dashboards => this.observer.next(dashboards));
+        if (this.observer) this.getReports().then(reports => this.observer.next(reports));
         return data;
     }
 
-    newDashboard(dashboard: Dashboard) {
-        return this.http.post('api/dashboards', classToPlain(dashboard))
+    newReport(report: Report) {
+        return this.http.post('api/dashboards', classToPlain(report))
             .toPromise()
             .then((data) => this.emitChange(data))
-			.then((data: object) => plainToClass(Dashboard, data))
+			.then((data: object) => plainToClass(Report, data))
             .catch(this.utils.handleResponseError)
     }
 
-    updateDashboard(dashboard: Dashboard) {
-        return this.http.put('api/dashboards', classToPlain(dashboard))
+    updateReport(report: Report) {
+        return this.http.put('api/dashboards', classToPlain(report))
             .toPromise()
             .then((data) => this.emitChange(data))
-			.then((data: object) => plainToClass(Dashboard, data))
+			.then((data: object) => plainToClass(Report, data))
 			.catch(this.utils.handleResponseError);
     }
 
-    deleteDashboard(dashboard: Dashboard) {
-        return this.http.delete(`/api/dashboards/${ dashboard.id }`)
+    deleteReport(report: Report) {
+        return this.http.delete(`/api/dashboards/${ report.id }`)
             .toPromise()
             .then((data) => this.emitChange(data))
             .catch(this.utils.handleResponseError);
     }
 
-    getDashboards() : Promise<Dashboard[]> {
+    getReports() : Promise<Report[]> {
         return this.http.get('/api/dashboards')
 			.toPromise()
-			.then((data) => plainToClass(Dashboard, data))
+			.then((data) => plainToClass(Report, data))
             .catch(this.utils.handleResponseError);
     }
 
-    getDashboard(id: string) : Promise<Dashboard> {
+    getReport(id: string) : Promise<Report> {
         return this.http.get(`/api/dashboards/${ id }`)
 			.toPromise()
-			.then((data: object) => plainToClass(Dashboard, data))
+			.then((data: object) => plainToClass(Report, data))
 			.catch(this.utils.handleResponseError);
     }
 
-    async getDashboardWithMetadata(id: string) : Promise<Dashboard> {
-        const theDashboard: Dashboard = await this.getDashboard(id);
-        await Promise.all(theDashboard.widgets.map(widget => this.getWidgetMetadata(widget)));
-        return theDashboard;
+    async getReportWithMetadata(id: string) : Promise<Report> {
+        const theReport: Report = await this.getReport(id);
+        await Promise.all(theReport.widgets.map(widget => this.getWidgetMetadata(widget)));
+        return theReport;
     }
 
-    async getWidgetMetadata(widget: DashboardWidget) {
+    async getWidgetMetadata(widget: ReportWidget) {
         const testData = "A,B,C\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6\n1,2,3\n4,5,6";
         return widget.setMetadata(await this.parseResult(testData))
 
