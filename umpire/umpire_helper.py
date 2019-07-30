35eeb9f1d1a303d71e674934bc815bd212726b24
emrodas10 <rodas_emilio@bah.com>
2019-07-30 14:38:33 -0400

    Added python functions for desired API endpoints

--- umpire/umpire_helper.py
+++ umpire/umpire_helper.py
@@ -8,16 +8,17 @@ import logging
 from common.docker_helpers import connect_to_aiodocker, docker_context, stream_docker_log, logger as docker_logger
 import pathlib
 
-tag_name = f"{static.APP_PREFIX}_hello_world"
-app_name = "hello_world"
-version = "1.0.0"
+# tag_name = f"{static.APP_PREFIX}_hello_world"
+# app_name = "hello_world"
+# version = "1.0.0"
 logger = logging.getLogger("Umpire API")
 
 
 class UmpireApi:
 
     @staticmethod
-    async def run():
+    async def build_image(app_name, version):
+        tag_name = f"{static.APP_PREFIX}_{app_name}"
         try:
             p_dest = pathlib.Path(f"./temp_apps/{app_name}/{version}/src").mkdir(parents=True, exist_ok=True)
         except Exception as e:
@@ -52,7 +53,42 @@ class UmpireApi:
                                                           path_dockerfile=dockerfile,
                                                           encoding="application/x-tar")
                 await stream_docker_log(log_stream)
+    @staticmethod
+    async def list_files(app_name, version):
+        relative_path = []
+        minio_client = Minio(config.MINIO, access_key='walkoff', secret_key='walkoff123', secure=False)
 
+        objects = minio_client.list_objects("apps-bucket", recursive=True)
+        for obj in objects:
+            p_src = Path(obj.object_name)
+            if p_src.parts[1] == app_name:
+                p_dst = p_src.relative_to(f"{p_src.parts[0]}/{app_name}/{version}")
+                relative_path.append(str(p_dst))
+        return relative_path
 
-if __name__ == "__main__":
-    asyncio.run(UmpireApi.run())
+    @staticmethod
+    async def get_file(app_name, version, path):
+        minio_client = Minio(config.MINIO, access_key='walkoff', secret_key='walkoff123', secure=False)
+        abs_path = f"apps/{app_name}/{version}/{path}"
+        data = minio_client.get_object('apps-bucket', abs_path)
+        return data.read()
+
+    @staticmethod
+    async def update_file(app_name, version, path, file_data, file_size):
+        minio_client = Minio(config.MINIO, access_key='walkoff', secret_key='walkoff123', secure=False)
+
+        abs_path = f"apps/{app_name}/{version}/{path}"
+        found = False
+        try:
+            minio_client.stat_object("apps-bucket", abs_path);
+            found = True;
+        except Exception as e:
+            print(e)
+
+        if found is True:
+            minio_client.remove_object("apps-bucket", abs_path)
+            minio_client.put_object("apps-bucket", abs_path, file_data, file_size)
+            print("Removed object")
+        else:
+            print("NOT FOUND")
+
