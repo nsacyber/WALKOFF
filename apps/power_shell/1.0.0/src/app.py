7116cad9c756fef3d7b5a4a539045905fbc38936
adpham95 <32873177+adpham95@users.noreply.github.com>
2019-08-19 11:24:08 -0400

    removed length limit on conditions and transforms, fixed powershell from script, fixed saving new files with new dir

--- apps/power_shell/1.0.0/src/app.py
+++ apps/power_shell/1.0.0/src/app.py
@@ -1,11 +1,13 @@
 import datetime
 import asyncio
-import os
+import logging
 
-from pypsrp.client import Process, SignalCode, WinRS
+from pypsrp.client import Process, SignalCode, WinRS, PowerShell as PS, RunspacePool
 from pypsrp.wsman import WSMan
 
 from walkoff_app_sdk.app_base import AppBase
+
+logging.getLogger("urllib3").setLevel(logging.ERROR)
 
 
 class PowerShell(AppBase):
@@ -36,11 +38,6 @@ class PowerShell(AppBase):
         """
 
         results = {}
-        curr_dir = os.getcwd()
-        temp_dir = os.path.join(curr_dir, r'scripts')
-        os.chdir(temp_dir)
-        curr_dir = os.getcwd()
-        local_file_path = os.path.join(curr_dir, local_file_name)
 
         for host in hosts:
             results[host] = ""
@@ -49,7 +46,7 @@ class PowerShell(AppBase):
                               username=username, password=password)
 
                 with WinRS(wsman) as shell:
-                    with open(local_file_path, "r") as f:
+                    with open(local_file_name, "r") as f:
                         script = f.read()
                     process = Process(shell, script)
                     process.invoke()
@@ -114,11 +111,6 @@ class PowerShell(AppBase):
         :return: dict of results with hosts as keys and list of outputs for each specified hosts
         """
         results = {}
-        curr_dir = os.getcwd()
-        temp_dir = os.path.join(curr_dir, r'scripts')
-        os.chdir(temp_dir)
-        curr_dir = os.getcwd()
-        local_file_path = os.path.join(curr_dir, local_file_name)
 
         for host in hosts:
             self.logger.info(f"Executing on {host}")
@@ -128,24 +120,27 @@ class PowerShell(AppBase):
                 wsman = WSMan(host, ssl=server_cert_validation, auth=transport, encryption=message_encryption,
                               username=username, password=password)
 
-                with WinRS(wsman) as shell:
-                    with open(local_file_path, "r") as f:
+                with RunspacePool(wsman) as pool:
+                    with open(local_file_name, "r") as f:
                         script = f.read()
 
-                    process = Process(shell, shell_type, script)
-                    process.begin_invoke()  # start the invocation and return immediately
-                    process.poll_invoke()  # update the output stream
-                    process.end_invoke()  # finally wait until the process is finished
-                    results[host] = {"stdout": process.stdout.decode(), "stderr": process.stderr.decode()}
-                    process.signal(SignalCode.CTRL_C)
+                    ps = PS(pool)
+                    ps.add_script(script)
+                    ps.invoke()
+
+                    if ps.had_errors:
+                        results[host] = {"stdout": "", "stderr": ps.output}
+                    else:
+                        results[host] = {"stdout": ps.output, "stderr": ""}
 
             except Exception as e:
                 results[host] = {"stdout": "", "stderr": f"{e}"}
 
         return results
 
-    async def exec_powershell_script(self, hosts, shell_type, arguments, username, password, transport, server_cert_validation,
-                              message_encryption):
+    async def exec_powershell_script(self, hosts, shell_type, arguments, username, password, transport,
+                                     server_cert_validation,
+                                     message_encryption):
         """
         Execute a list of remote commands on a list of hosts.
         :param hosts: List of host ips to run command on
@@ -182,7 +177,6 @@ class PowerShell(AppBase):
                 results[host] = {"stdout": "", "stderr": f"{e}"}
 
         return results
-
 
 if __name__ == "__main__":
     asyncio.run(PowerShell.run())
