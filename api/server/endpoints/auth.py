7377fa881a0bd833bb1c2264891d29a30b888833
adpham95 <32873177+adpham95@users.noreply.github.com>
2019-09-17 12:41:44 -0400

    turned problems into exceptions and updated auth with them

--- api/server/endpoints/auth.py
+++ api/server/endpoints/auth.py
@@ -1,39 +1,55 @@
 from starlette.requests import Request
 from fastapi import APIRouter, Depends
 from http import HTTPStatus
+import logging
+
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from api.security import (create_access_token, create_refresh_token, get_jwt_identity,
                           get_raw_jwt, decode_token, verify_jwt_refresh_token_in_request)
 from api.fastapi_config import FastApiConfig
-from api.server.utils.problem import Problem
 from api.server.db.user import User
 from api.server.db import get_db
 from api.server.db.tokens import revoke_token, AuthModel, TokenModel
+from api.server.utils.problems import ProblemException, InvalidInputException
 
 token_problem_title = 'Could not grant access token.'
-invalid_username_password_problem = Problem(
-    HTTPStatus.UNAUTHORIZED, token_problem_title, 'Invalid username or password.')
-user_deactivated_problem = Problem(HTTPStatus.UNAUTHORIZED, token_problem_title, 'User is deactivated.')
+
+invalid_credentials_problem = ProblemException(
+    HTTPStatus.UNAUTHORIZED,
+    token_problem_title,
+    'Invalid username or password.'
+)
+
+user_deactivated_problem = ProblemException(
+    HTTPStatus.UNAUTHORIZED,
+    token_problem_title,
+    'User is deactivated.'
+)
 
 router = APIRouter()
+logger = logging.getLogger("API")
 
 
 def _authenticate_and_grant_tokens(request: Request, db_session: Session, json_in: dict, with_refresh=False):
     username = json_in.get('username', None)
     password = json_in.get('password', None)
+    logger.info(f"{username}, {password}")
     if not (username and password):
-        return invalid_username_password_problem
+        raise invalid_credentials_problem
 
     user = db_session.query(User).filter_by(username=username).first()
     if user is None:
-        return invalid_username_password_problem
+        raise invalid_credentials_problem
     try:
         password = password.encode('utf-8')
     except UnicodeEncodeError:
-        return invalid_username_password_problem
+        raise invalid_credentials_problem
+
     if not user.active:
-        return user_deactivated_problem
+        raise user_deactivated_problem
+
     if user.verify_password(password):
         response = {'access_token': create_access_token(identity=user.id, db_session=db_session, fresh=True)}
         if with_refresh:
@@ -41,10 +57,10 @@ def _authenticate_and_grant_tokens(request: Request, db_session: Session, json_i
             # user.login(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
             db_session.commit()
             response['refresh_token'] = create_refresh_token(identity=user.id)
-        print(response)
-        return response, HTTPStatus.CREATED
+        # print(response)
+        return response
     else:
-        return invalid_username_password_problem
+        raise invalid_credentials_problem
 
 
 @router.post("/")
@@ -64,14 +80,14 @@ def refresh(request: Request, db_session: Session = Depends(get_db)):
     user = db_session.query(User).filter_by(id=current_user_id).first()
     if user is None:
         revoke_token(db_session=db_session, decoded_token=get_raw_jwt(request))
-        return Problem(
+        raise ProblemException(
             HTTPStatus.UNAUTHORIZED,
             "Could not grant access token.",
             f"User {current_user_id} from refresh JWT identity could not be found.")
     if user.active:
         return {'access_token': create_access_token(identity=current_user_id, db_session=db_session)}, HTTPStatus.CREATED
     else:
-        return user_deactivated_problem
+        raise user_deactivated_problem
 
 
 @router.post("/logout")
@@ -79,7 +95,7 @@ def logout(json_in: TokenModel, request: Request, db_session: Session = Depends(
     data = dict(json_in)
     refresh_token = data.get('refresh_token', None) if data else None
     if refresh_token is None:
-        return Problem(HTTPStatus.BAD_REQUEST, 'Could not logout.', 'A refresh token is required to logout.')
+        raise ProblemException(HTTPStatus.BAD_REQUEST, 'Could not logout.', 'A refresh token is required to logout.')
     decoded_refresh_token = decode_token(refresh_token)
     refresh_token_identity = decoded_refresh_token[FastApiConfig.JWT_IDENTITY_CLAIM]
     user_id = get_jwt_identity(request)
@@ -90,7 +106,7 @@ def logout(json_in: TokenModel, request: Request, db_session: Session = Depends(
         revoke_token(db_session=db_session, decoded_token=decode_token(refresh_token))
         return None, HTTPStatus.NO_CONTENT
     else:
-        return Problem(
+        raise ProblemException(
             HTTPStatus.BAD_REQUEST,
             'Could not logout.',
             'The identity of the refresh token does not match the identity of the authentication token.')
--- api/server/utils/problems.py
+++ api/server/utils/problems.py
@@ -1,14 +1,14 @@
 import json
 import logging
 
-from starlette.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from http import HTTPStatus
 
 logger = logging.getLogger("API")
 
 
-class Problem(JSONResponse):
+class ProblemException(HTTPException):
     """Returns a Problem Details object complying with RFC 7807
     .. https://tools.ietf.org/html/rfc7807
 
@@ -27,53 +27,98 @@ class Problem(JSONResponse):
     """
 
     def __init__(self, status, title, detail, instance=None, type_=None, ext=None, headers=None):
-        response = Problem.make_response_body(status, title, detail, instance, type_, ext)
-        JSONResponse.__init__(self, content=response, status_code=status, headers=headers)
+        # response = Problem.make_response_body(status, title, detail, instance, type_, ext)
+        super().__init__(status_code=status, detail=detail, headers=headers)
+        self.title = title
+        self.instance = instance
+        self.type = type_ if type_ else "about:blank"
+        self.ext = ext if ext is not None else {}
 
-    @staticmethod
-    def make_response_body(status, title, detail, instance=None, type_=None, ext=None):
-        if not type_:
-            type_ = "about:blank"
-
-        response = {"type": type_, "title": title, "detail": detail, "status": status}
-        if instance:
-            response["instance"] = instance
-        if ext:
-            response.update(ext)
-
-        return json.dumps(response)
-
-    @classmethod
-    def from_crud_resource(cls, status, resource, operation, detail, instance=None, type_=None, ext=None, headers=None):
-        title = f"Could not {operation} {resource}."
-        return cls(status, title, detail, instance=instance, type_=type_, ext=ext, headers=headers)
-
-
-def unique_constraint_problem(resource, operation, id_):
-    detail = f"Could not {operation} {resource} {id_}, possibly because of invalid or non-unique IDs"
-    logger.error(detail)
-    return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail)
-
-
-def improper_json_problem(resource, operation, id_, errors=None):
-    detail = f"Could not {operation} {resource} {id_}. Invalid JSON"
-    logger.error(f"{detail}. Details: {errors}")
-    return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail, ext={"errors": errors})
-
-
-def invalid_input_problem(resource, operation, id_, errors=None):
-    detail = f"Could not {operation} {resource} {id_}. Invalid input"
-    logger.error(f"{detail}. Details: {errors}")
-    return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail, ext={"errors": errors})
+    def as_dict(self):
+        r = {
+            "title": self.title,
+            "instance": self.instance,
+            "type": self.type,
+            "detail": self.detail,
+            "status": self.status_code
+        }
+        r.update(self.ext)
+        return r
+    #
+    # @staticmethod
+    # def make_response_body(status, title, detail, instance=None, type_=None, ext=None):
+    #     if not type_:
+    #         type_ = "about:blank"
+    #
+    #     response = {"type": type_, "title": title, "detail": detail, "status": status}
+    #     if instance:
+    #         response["instance"] = instance
+    #     if ext:
+    #         response.update(ext)
+    #
+    #     return response
+    #
+    # @classmethod
+    # def from_crud_resource(cls, status, resource, operation, detail, instance=None, type_=None, ext=None, headers=None):
+    #     title = f"Could not {operation} {resource}."
+    #     return cls(status, title, detail, instance=instance, type_=type_, ext=ext, headers=headers)
 
 
-def invalid_id_problem(resource, operation, id_):
-    detail = f"Could not {operation} {resource}. {id_} is an invalid ID"
-    logger.error(detail)
-    return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail)
+class UniquenessException(ProblemException):
+    def __init__(self, operation, resource, id_):
+        detail = f"Could not {operation} {resource} {id_}, possibly because of invalid or non-unique IDs."
+        super().__init__(HTTPStatus.BAD_REQUEST, "Uniqueness Constraint Failed.", detail=detail)
 
 
-def dne_problem(resource, operation, id_, errors=None):
-    detail = f"Could not {operation} {resource} {id_}. {resource.title()} does not exist"
-    logger.error(f"{detail}. Details: {errors}")
-    return Problem.from_crud_resource(HTTPStatus.NOT_FOUND, resource, operation, detail, ext=errors)
+class ImproperJSONException(ProblemException):
+    def __init__(self, operation, resource, id_, errors=None):
+        detail = f"Could not {operation} {resource} {id_}, invalid JSON."
+        super().__init__(HTTPStatus.BAD_REQUEST, "Improper JSON.", detail=detail, ext={"errors": errors})
+
+
+class InvalidInputException(ProblemException):
+    def __init__(self, operation, resource, id_):
+        detail = f"Could not {operation} {resource} {id_}, invalid input."
+        super().__init__(HTTPStatus.BAD_REQUEST, "Invalid Input.", detail=detail)
+
+
+class InvalidIDException(ProblemException):
+    def __init__(self, operation, resource, id_):
+        detail = f"Could not {operation} {resource} {id_}, invalid ID."
+        super().__init__(HTTPStatus.BAD_REQUEST, "Invalid ID.", detail=detail)
+
+
+class DoesNotExistException(ProblemException):
+    def __init__(self, operation, resource, id_):
+        detail = f"Could not {operation} {resource} {id_}, it does not exist."
+        super().__init__(HTTPStatus.BAD_REQUEST, "Uniqueness Constraint Failed.", detail=detail)
+
+
+# def unique_constraint_problem(resource, operation, id_):
+#     detail = f"Could not {operation} {resource} {id_}, possibly because of invalid or non-unique IDs"
+#     logger.error(detail)
+#     return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail)
+#
+#
+# def improper_json_problem(resource, operation, id_, errors=None):
+#     detail = f"Could not {operation} {resource} {id_}. Invalid JSON"
+#     logger.error(f"{detail}. Details: {errors}")
+#     return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail, ext={"errors": errors})
+#
+#
+# def invalid_input_problem(resource, operation, id_, errors=None):
+#     detail = f"Could not {operation} {resource} {id_}. Invalid input"
+#     logger.error(f"{detail}. Details: {errors}")
+#     return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail, ext={"errors": errors})
+#
+#
+# def invalid_id_problem(resource, operation, id_):
+#     detail = f"Could not {operation} {resource}. {id_} is an invalid ID"
+#     logger.error(detail)
+#     return Problem.from_crud_resource(HTTPStatus.BAD_REQUEST, resource, operation, detail)
+#
+#
+# def dne_problem(resource, operation, id_, errors=None):
+#     detail = f"Could not {operation} {resource} {id_}. {resource.title()} does not exist"
+#     logger.error(f"{detail}. Details: {errors}")
+#     return Problem.from_crud_resource(HTTPStatus.NOT_FOUND, resource, operation, detail, ext=errors)
