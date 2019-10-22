4eebfeae0c2f1dde1004083453b23e0f4cb4747b
hburke123 <burke_hannah@bah.com>
2019-10-22 10:13:55 -0400

    completed testing of settings; settings bug fix

--- testing/api/test_settings.py
+++ testing/api/test_settings.py
@@ -31,7 +31,7 @@ def test_update_settings(api: TestClient, auth_header: dict):
 
 
 def test_update_invalid_settings(api: TestClient, auth_header: dict):
-    p = api.get(base_settings_url, headers=auth_header)
+    api.get(base_settings_url, headers=auth_header)
 
     data = {
       "access_token_life_mins": 15,
@@ -40,3 +40,22 @@ def test_update_invalid_settings(api: TestClient, auth_header: dict):
 
     p = api.put(base_settings_url, headers=auth_header, data=json.dumps(data))
     assert p.status_code == 422
+
+
+def test_get_invalid_permission(api: TestClient, unauthorized_header: dict):
+    p = api.get(base_settings_url, headers=unauthorized_header)
+    assert p.status_code == 403
+
+
+def test_update_invalid_permission(api: TestClient, auth_header: dict, unauthorized_header: dict):
+    p = api.get(base_settings_url, headers=auth_header)
+    settings_id = p.json()["id_"]
+
+    data = {
+      "id_": settings_id,
+      "access_token_life_mins": 15,
+      "refresh_token_life_days": 15
+    }
+
+    p = api.put(base_settings_url, headers=unauthorized_header, data=json.dumps(data))
+    assert p.status_code == 403
