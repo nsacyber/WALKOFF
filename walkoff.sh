676f346d797cbcc67affb2bfe6c9be9d7318a794
emrodas10 <rodas_emilio@bah.com>
2020-01-23 16:26:21 -0500

    Adds checks to build process like ensuring we build from /WALKOFF directory and building bootloader when necessary. Fixed Dockerfile found in umpire so that it matches app_sdk.

--- walkoff.sh
+++ walkoff.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 BUILDARG="^-[a-zA-Z]*b|--build"
-if [[ $1 == "up" ]]; then
-  for var in "$@"
-  do
-    if [[ $var =~ $BUILDARG ]]; then
-      echo "Preparing WALKOFF Bootloader..."
-      docker build -t walkoff_bootloader -f bootloader/Dockerfile .
-      break
-    fi
-  done
+CLEANARG="^-[a-zA-Z]*c|--clean"
+WALK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ $WALK_DIR != $PWD ]; then
+  echo "Exiting because you aren't running the walkoff.sh script from within /WALKOFF"
+  exit
 fi
 
 echo "Starting WALKOFF Bootloader..."
+
+if [ ! "$(docker image ls | grep -w "walkoff_bootloader")" ]; then
+  echo "Building WALKOFF Bootloader as the image didn't exist..."
+  docker build -t walkoff_bootloader -f bootloader/Dockerfile $WALK_DIR
+fi
+
+for var in "$@"
+do
+  if [[ $var =~ $BUILDARG ]]; then
+    echo "Building WALKOFF Bootloader as you specified -b or --build..."
+    docker build -t walkoff_bootloader -f bootloader/Dockerfile $WALK_DIR
+    break
+  fi
+done
 
 if [ ! "$(docker network ls | grep -w "walkoff_network")" ]; then
   printf "Creating walkoff_network network: "
@@ -27,15 +38,11 @@ docker run --rm -it --network walkoff_network --name walkoff_bootloader \
     -w $(pwd) \
     walkoff_bootloader "$@"
 
-
-CLEANARG="^-[a-zA-Z]*c|--clean"
-if [[ $1 == "down" ]]; then
-  for var in "$@"
-  do
-    if [[ $var =~ $CLEANARG ]]; then
-      printf "Removing walkoff_network network: "
-      docker network rm walkoff_network
-      break
-    fi
-  done
-fi
+for var in "$@"
+do
+  if [[ $var =~ $CLEANARG ]]; then
+    printf "Removing walkoff_network network: "
+    docker network rm walkoff_network
+    break
+  fi
+done
